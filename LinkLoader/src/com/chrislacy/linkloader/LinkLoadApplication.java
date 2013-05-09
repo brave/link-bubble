3ca9792ef0d13031c2c3a4f012d3c0ce3b44eb46
Chris Lacy <gdaychris@gmail.com>
2013-05-10 09:50:59 +1000

    Add shell application

---
+++ LinkLoader/src/com/chrislacy/linkloader/LinkLoadApplication.java
@@ -0,0 +1,11 @@
+package com.chrislacy.linkloader;
+
+import android.app.Application;
+
+public class LinkLoadApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
