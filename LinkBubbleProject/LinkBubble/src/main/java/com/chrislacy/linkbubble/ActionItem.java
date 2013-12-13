65e690ca5bca1f45f6e906bdcd27bae88863c5f9
Chris Lacy <gdaychris@gmail.com>
2013-12-13 14:31:22 +1000

    Move creation of ActionItem dialog into its own file

---
+++ LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/ActionItem.java
@@ -0,0 +1,211 @@
+package com.chrislacy.linkbubble;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.preference.Preference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by chrislacy on 5/1/2013.
+ */
+public class ActionItem {
+
+    public static AlertDialog getConfigureBubbleAlert(Context context, final Config.BubbleAction bubble, final Preference preference) {
+
+        final ArrayList<Item> actionItems = new ArrayList<Item>();
+
+        String packageName = context.getPackageName();
+        PackageManager packageManager = context.getPackageManager();
+        Resources resources = context.getResources();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.fdasfjsadfdsfas.com"));        // Something stupid that no non-browser app will handle
+        List<ResolveInfo> resolveInfos = packageManager.queryIntentActivities (intent, PackageManager.GET_RESOLVED_FILTER);
+        for (ResolveInfo resolveInfo : resolveInfos) {
+            IntentFilter filter = resolveInfo.filter;
+            if (filter != null && filter.hasAction(Intent.ACTION_VIEW) && filter.hasCategory(Intent.CATEGORY_BROWSABLE)) {
+                // Ignore LinkBubble from this list
+                if (resolveInfo.activityInfo.packageName.equals(packageName) == false) {
+                    actionItems.add(new Item(Config.ActionType.View,
+                            resources,
+                            resolveInfo.loadLabel(packageManager).toString(),
+                            resolveInfo.loadIcon(packageManager),
+                            resolveInfo.activityInfo.packageName,
+                            resolveInfo.activityInfo.name));
+                }
+            }
+        }
+
+        // Get list of handler apps that can send
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        resolveInfos = packageManager.queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resolveInfos) {
+            actionItems.add(new Item(Config.ActionType.Share,
+                    resources,
+                    resolveInfo.loadLabel(packageManager).toString(),
+                    resolveInfo.loadIcon(packageManager),
+                    resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.name));
+        }
+
+        StickyListHeadersListView listView = new StickyListHeadersListView(context);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(R.string.preference_configure_bubble_title);
+        alertDialog.setView(listView);
+
+        Collections.sort(actionItems, new Comparator<Item>() {
+
+            @Override
+            public int compare(Item lhs, Item rhs) {
+                int categoryComparison = lhs.getCategory().compareTo(rhs.getCategory());
+                if (categoryComparison == 0) {
+                    return lhs.getLabel().compareTo(rhs.getLabel());
+                }
+                return categoryComparison;
+            }
+        });
+
+        ActionItemAdapter adapter = new ActionItemAdapter(context,
+                R.layout.action_picker_item,
+                actionItems.toArray(new Item[0]));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object tag = view.getTag();
+                if (tag instanceof Item) {
+                    Item actionItem = (Item) tag;
+                    Settings.get().setConsumeBubble(bubble, actionItem.mType, actionItem.getLabel(),
+                            actionItem.mPackageName, actionItem.mActivityClassName);
+                    preference.setSummary(Settings.get().getConsumeBubbleLabel(bubble));
+                    alertDialog.dismiss();
+                }
+            }
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.action_use_default), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+
+        });
+
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+            }
+        });
+
+        return alertDialog;
+    }
+
+    private static class ActionItemAdapter extends ArrayAdapter<Item> implements StickyListHeadersAdapter {
+
+        Context mContext;
+        int mLayoutResourceId;
+        Item mData[] = null;
+
+        public ActionItemAdapter(Context context, int layoutResourceId, Item[] data) {
+            super(context, layoutResourceId, data);
+            mLayoutResourceId = layoutResourceId;
+            mContext = context;
+            mData = data;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView==null){
+                LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+                convertView = inflater.inflate(mLayoutResourceId, parent, false);
+            }
+
+            Item actionItem = mData[position];
+
+            TextView label = (TextView) convertView.findViewById(R.id.label);
+            label.setText(actionItem.getLabel());
+
+            ImageView icon = (ImageView) convertView.findViewById(R.id.icon);
+            icon.setImageDrawable(actionItem.mIcon);
+
+            convertView.setTag(actionItem);
+
+            return convertView;
+        }
+
+        @Override
+        public View getHeaderView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            convertView = inflater.inflate(R.layout.section_header, parent, false);
+            Item actionItem = mData[position];
+            TextView headerLabel = (TextView)convertView.findViewById(R.id.section_text);
+            headerLabel.setText(actionItem.getCategory());
+            return convertView;
+        }
+
+        @Override
+        public long getHeaderId(int position) {
+            Item actionItem = mData[position];
+            if (actionItem.mType == Config.ActionType.View) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    };
+
+    private static class Item {
+
+        private String mLabel;
+        private Config.ActionType mType;
+        private String mCategory;
+        private String mPackageName;
+        private String mActivityClassName;
+        private Drawable mIcon;
+
+        public Item(Config.ActionType type, Resources resources, String label, Drawable icon, String packageName, String activityClassName) {
+            mType = type;
+            mLabel = label;
+            mCategory = resources.getString(type == Config.ActionType.View ? R.string.consume_category_view : R.string.consume_category_share);
+            mIcon = icon;
+            mPackageName = packageName;
+            mActivityClassName = activityClassName;
+        }
+
+        public String getLabel() {
+            return mLabel;
+        }
+
+        public String getCategory() {
+            return mCategory;
+        }
+    }
+
+}
--- LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/SettingsFragment.java
+++ LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/SettingsFragment.java
@@ -1,41 +1,15 @@
 package com.chrislacy.linkbubble;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Vector;
 
-import android.widget.TextView;
-
-import com.chrislacy.linkbubble.R;
-
-import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
-import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
  * Created by gw on 11/09/13.
@@ -229,7 +203,7 @@ public class SettingsFragment extends PreferenceFragment {
             leftConsumeBubblePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AlertDialog alertDialog = getConfigureBubbleAlert(Config.BubbleAction.ConsumeLeft, leftConsumeBubblePreference);
+                    AlertDialog alertDialog = ActionItem.getConfigureBubbleAlert(getActivity(),                            Config.BubbleAction.ConsumeLeft, leftConsumeBubblePreference);
                     alertDialog.show();
                     return true;
                 }
@@ -242,13 +216,15 @@ public class SettingsFragment extends PreferenceFragment {
             rightConsumeBubblePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AlertDialog alertDialog = getConfigureBubbleAlert(Config.BubbleAction.ConsumeRight, rightConsumeBubblePreference);
+                    AlertDialog alertDialog = ActionItem.getConfigureBubbleAlert(getActivity(),
+                            Config.BubbleAction.ConsumeRight, rightConsumeBubblePreference);
                     alertDialog.show();
                     return true;
                 }
             });
             rightConsumeBubblePreference.setSummary(Settings.get().getConsumeBubbleLabel(Config.BubbleAction.ConsumeRight));
         }
+
     }
 
     @Override
@@ -258,179 +234,4 @@ public class SettingsFragment extends PreferenceFragment {
         super.onDestroy();
     }
 
-    public AlertDialog getConfigureBubbleAlert(final Config.BubbleAction bubble, final Preference preference) {
-
-        final ArrayList<ActionItem> actionItems = new ArrayList<ActionItem>();
-
-        String packageName = getActivity().getPackageName();
-        PackageManager packageManager = getActivity().getPackageManager();
-        Resources resources = getActivity().getResources();
-
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://www.fdasfjsadfdsfas.com"));        // Something stupid that no non-browser app will handle
-        List<ResolveInfo> resolveInfos = packageManager.queryIntentActivities (intent, PackageManager.GET_RESOLVED_FILTER);
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            IntentFilter filter = resolveInfo.filter;
-            if (filter != null && filter.hasAction(Intent.ACTION_VIEW) && filter.hasCategory(Intent.CATEGORY_BROWSABLE)) {
-                // Ignore LinkBubble from this list
-                if (resolveInfo.activityInfo.packageName.equals(packageName) == false) {
-                    actionItems.add(new ActionItem(Config.ActionType.View,
-                                            resources,
-                                            resolveInfo.loadLabel(packageManager).toString(),
-                                            resolveInfo.loadIcon(packageManager),
-                                            resolveInfo.activityInfo.packageName,
-                                            resolveInfo.activityInfo.name));
-                }
-            }
-        }
-
-        // Get list of handler apps that can send
-        intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        resolveInfos = packageManager.queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            actionItems.add(new ActionItem(Config.ActionType.Share,
-                                            resources,
-                                            resolveInfo.loadLabel(packageManager).toString(),
-                                            resolveInfo.loadIcon(packageManager),
-                                            resolveInfo.activityInfo.packageName,
-                                            resolveInfo.activityInfo.name));
-        }
-
-        StickyListHeadersListView listView = new StickyListHeadersListView(getActivity());
-
-        final AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
-        alertDialog.setTitle(R.string.preference_configure_bubble_title);
-        alertDialog.setView(listView);
-
-        Collections.sort(actionItems, new Comparator<ActionItem>() {
-
-            @Override
-            public int compare(ActionItem lhs, ActionItem rhs) {
-                int categoryComparison = lhs.getCategory().compareTo(rhs.getCategory());
-                if (categoryComparison == 0) {
-                    return lhs.getLabel().compareTo(rhs.getLabel());
-                }
-                return categoryComparison;
-            }
-        });
-
-        ActionItemAdapter adapter = new ActionItemAdapter(getActivity(),
-                                                                    R.layout.action_picker_item,
-                                                                    actionItems.toArray(new ActionItem[0]));
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Object tag = view.getTag();
-                if (tag instanceof ActionItem) {
-                    ActionItem actionItem = (ActionItem) tag;
-                    Settings.get().setConsumeBubble(bubble, actionItem.mType, actionItem.getLabel(),
-                                                    actionItem.mPackageName, actionItem.mActivityClassName);
-                    preference.setSummary(Settings.get().getConsumeBubbleLabel(bubble));
-                    alertDialog.dismiss();
-                }
-            }
-        });
-
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.action_use_default), new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-            }
-
-        });
-
-        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-
-            @Override
-            public void onCancel(DialogInterface dialog) {
-            }
-        });
-
-        return alertDialog;
-    }
-
-    private class ActionItemAdapter extends ArrayAdapter<ActionItem> implements StickyListHeadersAdapter {
-
-        Context mContext;
-        int mLayoutResourceId;
-        ActionItem mData[] = null;
-
-        public ActionItemAdapter(Context context, int layoutResourceId, ActionItem[] data) {
-            super(context, layoutResourceId, data);
-            mLayoutResourceId = layoutResourceId;
-            mContext = context;
-            mData = data;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if(convertView==null){
-                LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
-                convertView = inflater.inflate(mLayoutResourceId, parent, false);
-            }
-
-            ActionItem actionItem = mData[position];
-
-            TextView label = (TextView) convertView.findViewById(R.id.label);
-            label.setText(actionItem.getLabel());
-
-            ImageView icon = (ImageView) convertView.findViewById(R.id.icon);
-            icon.setImageDrawable(actionItem.mIcon);
-
-            convertView.setTag(actionItem);
-
-            return convertView;
-        }
-
-        @Override
-        public View getHeaderView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
-            convertView = inflater.inflate(R.layout.section_header, parent, false);
-            ActionItem actionItem = mData[position];
-            TextView headerLabel = (TextView)convertView.findViewById(R.id.section_text);
-            headerLabel.setText(actionItem.getCategory());
-            return convertView;
-        }
-
-        @Override
-        public long getHeaderId(int position) {
-            ActionItem actionItem = mData[position];
-            if (actionItem.mType == Config.ActionType.View) {
-                return 0;
-            } else {
-                return 1;
-            }
-        }
-    };
-
-    private static class ActionItem {
-
-        private String mLabel;
-        private Config.ActionType mType;
-        private String mCategory;
-        private String mPackageName;
-        private String mActivityClassName;
-        private Drawable mIcon;
-
-        public ActionItem(Config.ActionType type, Resources resources, String label, Drawable icon, String packageName, String activityClassName) {
-            mType = type;
-            mLabel = label;
-            mCategory = resources.getString(type == Config.ActionType.View ? R.string.consume_category_view : R.string.consume_category_share);
-            mIcon = icon;
-            mPackageName = packageName;
-            mActivityClassName = activityClassName;
-        }
-
-        public String getLabel() {
-            return mLabel;
-        }
-
-        public String getCategory() {
-            return mCategory;
-        }
-    }
 }
