073ea530f8f4963869e23264b8576f93cb5cd2b1
Chris Lacy <gdaychris@gmail.com>
2013-12-18 18:02:06 +1000

    Initial HomeActivty, based on NoBoringActionBar project, re #40

---
+++ LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/HomeActivity.java
@@ -0,0 +1,213 @@
+package com.chrislacy.linkbubble;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.TypefaceSpan;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.chrislacy.linkbubble.R;
+import com.flavienlaurent.notboringactionbar.AlphaForegroundColorSpan;
+import com.flavienlaurent.notboringactionbar.KenBurnsView;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends Activity {
+
+    private static final String TAG = "HomeActivity";
+    private int mActionBarTitleColor;
+    private int mActionBarHeight;
+    private int mHeaderHeight;
+    private int mMinHeaderTranslation;
+    private ListView mListView;
+    private KenBurnsView mHeaderPicture;
+    private ImageView mHeaderLogo;
+    private View mHeader;
+    private View mPlaceHolderView;
+    private AccelerateDecelerateInterpolator mSmoothInterpolator;
+
+    private RectF mRect1 = new RectF();
+    private RectF mRect2 = new RectF();
+
+    private AlphaForegroundColorSpan mAlphaForegroundColorSpan;
+    private SpannableString mSpannableString;
+
+    private TypedValue mTypedValue = new TypedValue();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSmoothInterpolator = new AccelerateDecelerateInterpolator();
+        mHeaderHeight = getResources().getDimensionPixelSize(R.dimen.header_height);
+        mMinHeaderTranslation = -mHeaderHeight + getActionBarHeight();
+
+        setContentView(R.layout.activity_home);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        mHeader = findViewById(R.id.header);
+        mHeaderPicture = (KenBurnsView) findViewById(R.id.header_picture);
+        mHeaderPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
+        mHeaderLogo = (ImageView) findViewById(R.id.header_logo);
+
+        mActionBarTitleColor = getResources().getColor(R.color.actionbar_title_color);
+
+        mSpannableString = new SpannableString(getString(R.string.home_activity_label));
+        mAlphaForegroundColorSpan = new AlphaForegroundColorSpan(mActionBarTitleColor);
+
+        setupActionBar();
+        setupListView();
+    }
+
+    private void setupListView() {
+        ArrayList<String> FAKES = new ArrayList<String>();
+        for (int i = 0; i < 1000; i++) {
+            FAKES.add("entry " + i);
+        }
+        mPlaceHolderView = getLayoutInflater().inflate(R.layout.view_home_header, mListView, false);
+        mListView.addHeaderView(mPlaceHolderView);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FAKES));
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int scrollY = getScrollY();
+                //sticky actionbar
+                mHeader.setTranslationY(Math.max(-scrollY, mMinHeaderTranslation));
+                //header_logo --> actionbar icon
+                float ratio = clamp(mHeader.getTranslationY() / mMinHeaderTranslation, 0.0f, 1.0f);
+                interpolate(mHeaderLogo, getActionBarIconView(), mSmoothInterpolator.getInterpolation(ratio));
+                //actionbar title alpha
+                //getActionBarTitleView().setAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
+                //---------------------------------
+                //better way thanks to @cyrilmottier
+                setTitleAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
+            }
+        });
+    }
+
+    private void setTitleAlpha(float alpha) {
+        mAlphaForegroundColorSpan.setAlpha(alpha);
+        mSpannableString.setSpan(mAlphaForegroundColorSpan, 0, mSpannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getActionBar().setTitle(mSpannableString);
+    }
+
+    public static float clamp(float value, float max, float min) {
+        return Math.max(Math.min(value, min), max);
+    }
+
+    private void interpolate(View view1, View view2, float interpolation) {
+        getOnScreenRect(mRect1, view1);
+        getOnScreenRect(mRect2, view2);
+
+        float scaleX = 1.0F + interpolation * (mRect2.width() / mRect1.width() - 1.0F);
+        float scaleY = 1.0F + interpolation * (mRect2.height() / mRect1.height() - 1.0F);
+        float translationX = 0.5F * (interpolation * (mRect2.left + mRect2.right - mRect1.left - mRect1.right));
+        float translationY = 0.5F * (interpolation * (mRect2.top + mRect2.bottom - mRect1.top - mRect1.bottom));
+
+        view1.setTranslationX(translationX);
+        view1.setTranslationY(translationY - mHeader.getTranslationY());
+        view1.setScaleX(scaleX);
+        view1.setScaleY(scaleY);
+    }
+
+    private RectF getOnScreenRect(RectF rect, View view) {
+        rect.set(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+        return rect;
+    }
+
+    public int getScrollY() {
+        View c = mListView.getChildAt(0);
+        if (c == null) {
+            return 0;
+        }
+
+        int firstVisiblePosition = mListView.getFirstVisiblePosition();
+        int top = c.getTop();
+
+        int headerHeight = 0;
+        if (firstVisiblePosition >= 1) {
+            headerHeight = mPlaceHolderView.getHeight();
+        }
+
+        return -top + firstVisiblePosition * c.getHeight() + headerHeight;
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setIcon(R.drawable.ic_transparent);
+
+        //getActionBarTitleView().setAlpha(0f);
+    }
+
+    private ImageView getActionBarIconView() {
+        return (ImageView) findViewById(android.R.id.home);
+    }
+
+    /*private TextView getActionBarTitleView() {
+        int id = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
+        return (TextView) findViewById(id);
+    }*/
+
+    public int getActionBarHeight() {
+        if (mActionBarHeight != 0) {
+            return mActionBarHeight;
+        }
+        getTheme().resolveAttribute(android.R.attr.actionBarSize, mTypedValue, true);
+        mActionBarHeight = TypedValue.complexToDimensionPixelSize(mTypedValue.data, getResources().getDisplayMetrics());
+        return mActionBarHeight;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_activity, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                startActivityForResult(new Intent(this, SettingsActivity.class), 0);
+                return true;
+            }
+
+            case R.id.action_upgrade_to_pro: {
+                Intent intent = Config.getStoreIntent(this, Config.STORE_PRO_URL);
+                if (intent != null) {
+                    startActivity(intent);
+                    return true;
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
