a2a12a32b1ca0f83c822fd2081735b4320232a1c
Chris Lacy <gdaychris@gmail.com>
2013-12-30 12:21:40 +1000

    YouTube embed select progress, re #91

--- LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/YouTubeEmbedHelper.java
+++ LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/YouTubeEmbedHelper.java
@@ -1,7 +1,12 @@
 package com.chrislacy.linkbubble;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.ResolveInfo;
+import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,10 +62,28 @@ public class YouTubeEmbedHelper {
     }
 
     boolean onOpenInAppButtonClick() {
-        if (mEmbedIds.size() > 0) {
+        int size = mEmbedIds.size();
+        if (size == 1) {
             return loadYouTubeVideo(mEmbedIds.get(0));
+        } else if (size > 1) {
+            getMultipleEmbedsDialog().show();
+            return true;
         }
 
         return false;
+    }
+
+    private AlertDialog getMultipleEmbedsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+        final ProgressBar progressBar = new ProgressBar(mContext);
+
+        builder.setView(progressBar);
+        builder.setIcon(0);
+        builder.setTitle(R.string.title_youtube_embed_to_load);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        return alertDialog;
     }
 }
