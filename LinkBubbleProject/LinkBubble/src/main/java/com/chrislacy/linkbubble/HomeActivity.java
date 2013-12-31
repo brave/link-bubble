f939966989145444af9c551052a389ae0ab6fcec
Chris Lacy <gdaychris@gmail.com>
2013-12-31 16:10:04 +1000

    Change HomeActivity to use just a single cycling image, re #55
    - By no means happy with this, and it won’t ship. But I’m adding it in
    the hope of spurring me to think of something better…

--- LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/HomeActivity.java
+++ LinkBubbleProject/LinkBubble/src/main/java/com/chrislacy/linkbubble/HomeActivity.java
@@ -1,54 +1,14 @@
 package com.chrislacy.linkbubble;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.Color;
-import android.graphics.RectF;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.TypefaceSpan;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.PopupMenu;
-import android.widget.TextView;
 import at.technikum.mti.fancycoverflow.FancyCoverFlow;
-import com.chrislacy.linkbubble.R;
-import com.flavienlaurent.notboringactionbar.AlphaForegroundColorSpan;
 import com.flavienlaurent.notboringactionbar.KenBurnsView;
-import com.google.android.apps.dashclock.ui.SwipeDismissListViewTouchListener;
-import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Vector;
 
 public class HomeActivity extends Activity {
@@ -63,7 +23,7 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
 
         mHeaderPicture = (KenBurnsView) findViewById(R.id.header_picture);
-        mHeaderPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
+        mHeaderPicture.setResourceIds(R.drawable.the_internet, R.drawable.the_internet);
 
         if (Settings.get().debugAutoLoadUrl()) {
             MainApplication.openLink(this, "http://abc.net.au");
