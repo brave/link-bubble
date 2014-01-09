0bd63dfa487b838f44641f77549c9dba52406a30
Chris Lacy <gdaychris@gmail.com>
2014-01-09 17:06:23 +1000

    Initial BubbleDraggable, re #71

--- Application/LinkBubble/src/main/java/com/linkbubble/MainControllerNew.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/MainControllerNew.java
@@ -6,6 +6,7 @@ import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import com.linkbubble.physics.Draggable;
 import com.linkbubble.physics.DraggableHelper;
+import com.linkbubble.ui.BubbleDraggable;
 import com.linkbubble.ui.BubblePagerDraggable;
 
 
@@ -19,6 +20,7 @@ public class MainControllerNew extends MainController {
     }
 
     private BubblePagerDraggable mBubblePagerDraggable;
+    private BubbleDraggable mBubbleDraggable;
 
     protected MainControllerNew(Context context, EventHandler eventHandler) {
         super(context, eventHandler);
@@ -27,24 +29,29 @@ public class MainControllerNew extends MainController {
         mBubblePagerDraggable = (BubblePagerDraggable) inflater.inflate(R.layout.view_bubble_pager, null);
 
         int bubbleFlowViewX = (Config.mScreenWidth - context.getResources().getDimensionPixelSize(R.dimen.bubble_pager_width)) / 2;
-        mBubblePagerDraggable.configure(bubbleFlowViewX, 0, bubbleFlowViewX, 0, 0.f, new BubblePagerDraggable.EventHandler() {
+        mBubblePagerDraggable.configure(bubbleFlowViewX, 0, bubbleFlowViewX, 0, 0.f, null);
+
+        mBubbleDraggable = (BubbleDraggable) inflater.inflate(R.layout.view_bubble_draggable, null);
+        mBubbleDraggable.configure((int) (Config.mBubbleSnapLeftX - Config.mBubbleWidth), Config.BUBBLE_HOME_Y,
+                Config.BUBBLE_HOME_X, Config.BUBBLE_HOME_Y, 0.4f, new BubbleDraggable.EventHandler() {
             @Override
-            public void onMotionEvent_Touch(BubblePagerDraggable sender, DraggableHelper.TouchEvent event) {
+            public void onMotionEvent_Touch(BubbleDraggable sender, DraggableHelper.TouchEvent event) {
                 mCurrentState.onTouchActionDown(sender, event);
             }
 
             @Override
-            public void onMotionEvent_Move(BubblePagerDraggable sender, DraggableHelper.MoveEvent event) {
+            public void onMotionEvent_Move(BubbleDraggable sender, DraggableHelper.MoveEvent event) {
                 mCurrentState.onTouchActionMove(sender, event);
             }
 
             @Override
-            public void onMotionEvent_Release(BubblePagerDraggable sender, DraggableHelper.ReleaseEvent event) {
+            public void onMotionEvent_Release(BubbleDraggable sender, DraggableHelper.ReleaseEvent event) {
                 mCurrentState.onTouchActionRelease(sender, event);
             }
         });
-        mDraggables.add(mBubblePagerDraggable);
-        setActiveDraggable(mBubblePagerDraggable);
+
+        mDraggables.add(mBubbleDraggable);
+        setActiveDraggable(mBubbleDraggable);
     }
 
     @Override
