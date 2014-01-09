030f7f3243279eb6c69a5e732c3648e37c6e0d0a
Chris Lacy <gdaychris@gmail.com>
2014-01-09 16:08:09 +1000

    Remove dependency on FancyCoverFlow, re #71

--- Application/LinkBubble/src/main/java/com/linkbubble/ui/HomeActivity.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/HomeActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
-import at.technikum.mti.fancycoverflow.FancyCoverFlow;
 import com.linkbubble.Constant;
 import com.linkbubble.MainApplication;
 import com.linkbubble.MainController;
