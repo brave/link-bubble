6e2cd8b0dffc2d24b3f7a443e23e87b78b901317
Chris Lacy <gdaychris@gmail.com>
2014-01-29 15:30:25 +1000

    Add placeholder IS_LICENSED flag, re #6

--- Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
@@ -5,6 +5,8 @@ public class Constant {
 
     public static final String INTENT_EXTRA_IGNORE_LAUNCH_ANIMATION = "com.android.launcher.intent.extra.shortcut.INGORE_LAUNCH_ANIMATION";
 
+    public static boolean IS_LICENSED = false;
+
     public static final int BUBBLE_ANIM_TIME = 300;
 
     public static final String PRIVACY_POLICY_URL = "http://www.actionlauncher.com/privacy";
