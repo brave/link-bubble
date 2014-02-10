cc1e9266caa265abaffd3f640cc745f833868239
Chris Lacy <gdaychris@gmail.com>
2014-02-11 09:01:00 +1000

    HomeActivity configured for Crashlytics

--- Application/LinkBubble/src/main/java/com/linkbubble/ui/HomeActivity.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/HomeActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
+import com.crashlytics.android.Crashlytics;
 import com.linkbubble.Config;
 import com.linkbubble.Constant;
 import com.linkbubble.DRM;
@@ -43,6 +44,7 @@ public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
 
         setContentView(R.layout.activity_home);
 
