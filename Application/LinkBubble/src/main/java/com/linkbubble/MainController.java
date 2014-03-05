6b62d6147c1cea0a966c5509ce96e871c97ebcdf
Glenn Watson <gw@intuitionlibrary.com>
2014-03-06 05:41:46 +1000

    Ensure bubbles are visible after sleep event. Please see note below. Ref #415.

--- Application/LinkBubble/src/main/java/com/linkbubble/MainController.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/MainController.java
@@ -799,6 +799,7 @@ public class MainController implements Choreographer.FrameCallback {
         if (action.equals(Intent.ACTION_SCREEN_OFF)) {
             setCanDisplay(false);
         } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
+            updateKeyguardLocked();
         } else if (action.equals(Intent.ACTION_USER_PRESENT)) {
             setCanDisplay(true);
         }
