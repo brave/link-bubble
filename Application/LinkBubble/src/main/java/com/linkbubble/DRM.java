f0669f1edce1259a0bcb2c098dec3ece0fdd8a5c
Chris Lacy <gdaychris@gmail.com>
2014-03-10 20:01:58 +1000

    Fix incorrectly named variable

--- Application/LinkBubble/src/main/java/com/linkbubble/DRM.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/DRM.java
@@ -80,7 +80,7 @@ public class DRM {
 
     private ServiceInfo getProServiceInfo(Context context) {
         final Intent mainIntent = new Intent(Constant.PRO_DRM_SERVICE_ACTION, null);
-        mainIntent.setPackage(BuildConfig.PRO_LAUNCHER_PACKAGE_NAME);
+        mainIntent.setPackage(BuildConfig.PRO_PACKAGE_NAME);
         List<ResolveInfo> services = context.getPackageManager().queryIntentServices(mainIntent, 0);
         if (services != null && services.size() > 0) {
             Log.d(TAG, "getProServiceInfo() - " + services.get(0).serviceInfo.name);
