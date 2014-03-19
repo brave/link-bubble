c3d2a78b521d902a92b8c8956d69a61388be9b4f
Chris Lacy <gdaychris@gmail.com>
2014-03-19 11:51:22 +1000

    Initial GPS position requesting, re #432

--- Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
@@ -88,4 +88,6 @@ public class Constant {
 
     public static final String TWITTER_ACCOUNT_TYPE = "com.twitter.android.auth.login";
     public static final String YAHOO_ACCOUNT_TYPE = "com.yahoo.mobile.client.share.account";
+
+    public static final String WEBVIEW_DATABASE_LOCATION = "/data/data/" + BuildConfig.PACKAGE_NAME;
 }
