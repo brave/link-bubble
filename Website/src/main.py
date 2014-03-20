605d0048e2be08e390552fd807e20caaa0cd76ca
Chris Lacy <gdaychris@gmail.com>
2014-03-20 16:46:05 +1000

    Remove temp code for website

--- Website/src/main.py
+++ Website/src/main.py
@@ -6,43 +6,25 @@ import appdef
 class PrivacyHandler(BaseHandler):
     
     def get(self):
-        allow = self.request.get("yes_let_me_in_20")
-        if allow == "whynot10":
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'privacy_policy.html', None)
-        else:
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'soon.html', None)
+        self.write_template(appdef.TEMPLATE_ROOT_PATH + 'privacy_policy.html', None)
 
 #==============================================================================
 class TermsHandler(BaseHandler):
     
     def get(self):
-        allow = self.request.get("yes_let_me_in_20")
-        if allow == "whynot10":
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'terms.html', None)
-        else:
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'soon.html', None)
+        self.write_template(appdef.TEMPLATE_ROOT_PATH + 'terms.html', None)
 
 #==============================================================================
 class WelcomeHandler(BaseHandler):
     
     def get(self):
-        allow = self.request.get("yes_let_me_in_20")
-        if allow == "whynot10":
-            self.redirect("https://s3.amazonaws.com/linkbubble/welcome.html")
-        else:
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'soon.html', None)
-
+        self.redirect("https://s3.amazonaws.com/linkbubble/welcome.html")
 
 #==============================================================================
 class MainPage(BaseHandler):
     
     def get(self):
-        allow = self.request.get("yes_let_me_in_20")
-        if allow == "whynot10":
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'home.html', None)
-        else:
-            self.write_template(appdef.TEMPLATE_ROOT_PATH + 'soon.html', None)
-
+        self.write_template(appdef.TEMPLATE_ROOT_PATH + 'home.html', None)
 
 #==============================================================================
 application = webapp2.WSGIApplication([('/privacy', PrivacyHandler),
