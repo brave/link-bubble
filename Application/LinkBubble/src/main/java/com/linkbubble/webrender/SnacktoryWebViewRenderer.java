99f034a89a5b540a217b4be2d58de6ca9153bffb
Chris Lacy <gdaychris@gmail.com>
2014-04-29 18:13:35 +1000

    Integration of WebView controls for SnacktoryWebViewRenderer

--- Application/LinkBubble/src/main/java/com/linkbubble/webrender/SnacktoryWebViewRenderer.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/webrender/SnacktoryWebViewRenderer.java
@@ -3,14 +3,20 @@ package com.linkbubble.webrender;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.gson.annotations.SerializedName;
@@ -44,7 +50,12 @@ public class SnacktoryWebViewRenderer extends WebRenderer {
 
     private GetPageAsTextTask mGetPageAsTextTask;
     private WebView mWebView;
+    private View mTouchInterceptorView;
+    private long mLastWebViewTouchUpTime = -1;
+    private String mLastWebViewTouchDownUrl;
     private TouchIconTransformation mTouchIconTransformation;
+    private Boolean mIsDestroyed = false;
+    private boolean mDoDropDownCheck;
 
     public SnacktoryWebViewRenderer(Context context, Controller controller, View webRendererPlaceholder, String tag) {
         super(context, controller, webRendererPlaceholder);
@@ -52,11 +63,25 @@ public class SnacktoryWebViewRenderer extends WebRenderer {
         mWebView = new WebView(context);
         mWebView.setLayoutParams(webRendererPlaceholder.getLayoutParams());
         Util.replaceViewAtPosition(webRendererPlaceholder, mWebView);
+
+        mTouchInterceptorView = new View(context);
+        mTouchInterceptorView.setLayoutParams(webRendererPlaceholder.getLayoutParams());
+        mTouchInterceptorView.setWillNotDraw(true);
+        mTouchInterceptorView.setOnTouchListener(mWebViewOnTouchListener);
+
+        ViewGroup parent = (ViewGroup)mWebView.getParent();
+        int index = parent.indexOfChild(mWebView);
+        parent.addView(mTouchInterceptorView, index+1);
+
+        //mWebView.setWebViewClient(mWebViewClient);
+        mWebView.setOnLongClickListener(mOnWebViewLongClickListener);
+        mWebView.setOnKeyListener(mOnKeyListener);
     }
 
     @Override
     public void destroy() {
-
+        mIsDestroyed = true;
+        mWebView.destroy();
     }
 
     @Override
@@ -160,6 +185,9 @@ public class SnacktoryWebViewRenderer extends WebRenderer {
             try {
                 setUrl(urlAsString);
 
+                //mWebView.loadUrl(urlAsString);
+                //mWebView.stopLoading();
+
                 mWebView.loadData(pageHtml, "text/html", "utf-8");
 
                 if (title != null) {
@@ -182,6 +210,114 @@ public class SnacktoryWebViewRenderer extends WebRenderer {
             }
         }
     }
+
+    View.OnKeyListener mOnKeyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && mIsDestroyed == false) {
+                WebView webView = (WebView) v;
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_BACK: {
+                        return mController.onBackPressed();
+                    }
+                }
+            }
+
+            return false;
+        }
+    };
+
+    View.OnLongClickListener mOnWebViewLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            WebView.HitTestResult hitTestResult = mWebView.getHitTestResult();
+            //Log.d(TAG, "onLongClick type: " + hitTestResult.getType());
+            switch (hitTestResult.getType()) {
+                case WebView.HitTestResult.SRC_ANCHOR_TYPE:
+                case WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE: {
+                    final String url = hitTestResult.getExtra();
+                    if (url == null) {
+                        return false;
+                    }
+
+                    mController.onUrlLongClick(url);
+                    return true;
+                }
+
+                case WebView.HitTestResult.UNKNOWN_TYPE:
+                default:
+                    mController.onShowBrowserPrompt();
+                    return false;
+            }
+        }
+    };
+
+    private View.OnTouchListener mWebViewOnTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            final int action = event.getAction() & MotionEvent.ACTION_MASK;
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    mLastWebViewTouchDownUrl = mUrl.toString();
+                    //Log.d(TAG, "[urlstack] WebView - MotionEvent.ACTION_DOWN");
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    mLastWebViewTouchUpTime = System.currentTimeMillis();
+                    //Log.d(TAG, "[urlstack] WebView - MotionEvent.ACTION_UP");
+                    break;
+            }
+            // Forcibly pass along to the WebView. This ensures we receive the ACTION_UP event above.
+            mWebView.onTouchEvent(event);
+            return true;
+        }
+    };
+
+    WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView wView, final String urlAsString) {
+            boolean viaInput = false;
+            if (mLastWebViewTouchUpTime > -1) {
+                long touchUpTimeDelta = System.currentTimeMillis() - mLastWebViewTouchUpTime;
+                // this value needs to be largish
+                if (touchUpTimeDelta < 1500) {
+                    // If the url has changed since the use pressed their finger down, a redirect has likely occurred,
+                    // in which case we don't update the Url Stack
+                    if (mLastWebViewTouchDownUrl.equals(mUrl.toString())) {
+                        viaInput = true;
+                    }
+                    mLastWebViewTouchUpTime = -1;
+
+                }
+            }
+
+            if (viaInput) {
+                mDoDropDownCheck = true;
+            }
+
+            return mController.shouldOverrideUrlLoading(urlAsString, viaInput);
+        }
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            mController.onReceivedError();
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String urlAsString, Bitmap favIcon) {
+            mDoDropDownCheck = true;
+            mController.onPageStarted(urlAsString, favIcon);
+        }
+
+        @Override
+        public void onPageFinished(WebView webView, String urlAsString) {
+            mController.onPageFinished(urlAsString);
+        }
+    };
 
 
     private static class TouchIconTransformation implements Transformation {
