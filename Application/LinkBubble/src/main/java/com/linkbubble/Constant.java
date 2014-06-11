1b9226c8181dd9d4b72cceb8e8eb58c646c7fcf3
Chris Lacy <gdaychris@gmail.com>
2014-06-11 11:42:46 +1000

    Enable Article Mode

--- Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
@@ -17,7 +17,7 @@ public class Constant {
         Share,
     }
 
-    public static final boolean ARTICLE_MODE = false;
+    public static final boolean ARTICLE_MODE = true;
 
     // Make GW's changes per d46678694ab79ed7a4aec5e293beff9ae9a62382 optional
     public static final boolean DYNAMIC_ANIM_STEP = true;
