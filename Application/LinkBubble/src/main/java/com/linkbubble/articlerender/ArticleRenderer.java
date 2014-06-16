5374be0c04a887fb44befa9b38b9f9fcfee4e9aa
Chris Lacy <gdaychris@gmail.com>
2014-06-16 20:15:37 +1000

    Back button progress for ArticleRenderer
    - Add initial ArticleRenderer.Controller.
    - Move functionality used by ArticleRenderer.Controller and
    WebRenderer.Controller into a common space

--- Application/LinkBubble/src/main/java/com/linkbubble/articlerender/ArticleRenderer.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/articlerender/ArticleRenderer.java
@@ -2,20 +2,39 @@ package com.linkbubble.articlerender;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.DownloadListener;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import com.linkbubble.Constant;
 import com.linkbubble.util.Util;
+import com.linkbubble.webrender.WebRenderer;
 
 
 public class ArticleRenderer {
 
+    public interface Controller {
+        public void onUrlLongClick(String url);
+        public void onDownloadStart(String urlAsString);
+        public boolean onBackPressed();
+        public void onShowBrowserPrompt();
+    }
+    
     private WebView mWebView;
+    private boolean mIsDestroyed = false;
+    private Controller mController;
 
-    public ArticleRenderer(Context context, ArticleContent articleContent, View articleRendererPlaceholder) {
+    public ArticleRenderer(Context context, Controller controller, ArticleContent articleContent, View articleRendererPlaceholder) {
+        mController = controller;
+
         mWebView = new WebView(context);
         mWebView.setLayoutParams(articleRendererPlaceholder.getLayoutParams());
         Util.replaceViewAtPosition(articleRendererPlaceholder, mWebView);
+
+        mWebView.setDownloadListener(mDownloadListener);
+        mWebView.setOnLongClickListener(mOnWebViewLongClickListener);
+        mWebView.setOnKeyListener(mOnKeyListener);
 
         display(articleContent, false);
 
@@ -34,6 +53,7 @@ public class ArticleRenderer {
     }
 
     public void destroy() {
+        mIsDestroyed = true;
         if (mWebView != null) {
             mWebView.destroy();
             Log.d("Article", "ArticleRenderer.destroy()");
@@ -49,5 +69,57 @@ public class ArticleRenderer {
             mWebView.stopLoading();
         }
     }
+
+    DownloadListener mDownloadListener = new DownloadListener() {
+        @Override
+        public void onDownloadStart(String urlAsString, String userAgent,
+                                    String contentDisposition, String mimetype,
+                                    long contentLength) {
+            mController.onDownloadStart(urlAsString);
+        }
+    };
+
+    View.OnLongClickListener mOnWebViewLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            WebView.HitTestResult hitTestResult = mWebView.getHitTestResult();
+            //Log.d(TAG, "onLongClick type: " + hitTestResult.getType());
+            switch (hitTestResult.getType()) {
+                case WebView.HitTestResult.SRC_ANCHOR_TYPE:
+                case WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE: {
+                    final String url = hitTestResult.getExtra();
+                    if (url == null) {
+                        return false;
+                    }
+
+                    mController.onUrlLongClick(url);
+                    return true;
+                }
+
+                case WebView.HitTestResult.UNKNOWN_TYPE:
+                default:
+                    if (Constant.SELECT_TEXT_VIA_ACTIVITY == false) {
+                        mController.onShowBrowserPrompt();
+                    }
+                    return false;
+            }
+        }
+    };
+
+    View.OnKeyListener mOnKeyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && mIsDestroyed == false) {
+                WebView webView = (WebView) v;
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_BACK: {
+                        return mController.onBackPressed();
+                    }
+                }
+            }
+
+            return false;
+        }
+    };
 
 }
