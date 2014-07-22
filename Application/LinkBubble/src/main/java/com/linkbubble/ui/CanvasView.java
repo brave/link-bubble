brave/link-bubble

commit cb4c5cadd7fbda11774314369f9d25fd330d389a

Only apply CanvasView alpha on masks
- Done as an optimization and to closer match L’s slide on animation.
- Don’t setVisibility(GONE) if the ContentView is animating off

Application/LinkBubble/src/main/java/com/linkbubble/ui/CanvasView.java
@@ -37,6 +37,7 @@ public class CanvasView extends FrameLayout {
     private final float mMaxAlpha = 1.0f;
     private final float mFadeTime = 0.3f;
     private final float mAlphaDelta = mMaxAlpha / mFadeTime;
+    //private final float mAlphaDelta = 1.33f * (mMaxAlpha / (Constant.BUBBLE_ANIM_TIME / 1000.f));
 
     private float mCurrentAlpha = 0.0f;
     private float mTargetAlpha = 0.0f;
@@ -93,8 +94,6 @@ public class CanvasView extends FrameLayout {
 
             MainController.addRootWindow(mStatusBarCoverView, lp);
         }
-
-        applyAlpha();
 
         Resources resources = getResources();
         LayoutInflater inflater = LayoutInflater.from(context);
@@ -158,6 +157,8 @@ public class CanvasView extends FrameLayout {
         mBottomMaskView.setLayoutParams(bottomMaskLP);
         addView(mBottomMaskView);
 
+        applyAlpha();
+
         mWindowManagerParams.gravity = Gravity.TOP | Gravity.LEFT;
         mWindowManagerParams.x = 0;
         mWindowManagerParams.y = 0;
@@ -201,9 +202,11 @@ public class CanvasView extends FrameLayout {
         if (mStatusBarCoverView != null) {
             mStatusBarCoverView.setAlpha(mCurrentAlpha);
         }
-        setAlpha(mCurrentAlpha);
 
-        if (!mEnabled || mCurrentAlpha == 0.0f) {
+        mTopMaskView.setAlpha(mCurrentAlpha);
+        mBottomMaskView.setAlpha(mCurrentAlpha);
+
+        if (!mEnabled || (mCurrentAlpha == 0.0f && mContentViewY == (int)(mTargetY))) {
             setVisibility(GONE);
             if (mStatusBarCoverView != null) {
                 mStatusBarCoverView.setVisibility(GONE);

commit 1fefbdac4734fce7302a479b5b1c280b24e3d4f7

Ensure CanvasView is hidden initially.
Fixes issue introduced in cb4c5ca

Application/LinkBubble/src/main/java/com/linkbubble/ui/CanvasView.java
@@ -75,6 +75,7 @@ public class CanvasView extends FrameLayout {
 
         mEnabled = true;
         mContentViewY = Config.mScreenHeight - Config.mContentOffset;
+        mTargetY = mContentViewY;
         int canvasMaskHeight = getResources().getDimensionPixelSize(R.dimen.canvas_mask_height);
 
         if (Constant.COVER_STATUS_BAR) {