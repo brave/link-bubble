b6e24dc133a382944421d26378d4dc83db1ba731
Chris Lacy <gdaychris@gmail.com>
2014-07-29 10:54:16 +1000

    Fix wrong button notification action being sent
    - By splitting into a new Activity for each action, no long experience
    these hiccups

---
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/NotificationHideActivity.java
@@ -0,0 +1,30 @@
+package com.linkbubble.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.linkbubble.BuildConfig;
+import com.linkbubble.MainController;
+import com.linkbubble.util.CrashTracking;
+
+public class NotificationHideActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        CrashTracking.init(this);
+
+        MainController mainController = MainController.get();
+
+        if (mainController != null) {
+            //Log.d("blerg", "*** handle Hide");
+            mainController.saveCurrentTabs();
+            mainController.setHiddenByUser(true);
+        }
+
+        finish();
+    }
+
+}
