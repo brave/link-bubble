066afbb241f033c2058ad0b76125316ac4680886
Chris Lacy <gdaychris@gmail.com>
2014-07-29 14:42:41 +1000

    Add "Open on phone" functionality to Wear notification

---
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/NotificationOpenTabActivity.java
@@ -0,0 +1,35 @@
+package com.linkbubble.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.linkbubble.BuildConfig;
+import com.linkbubble.MainController;
+import com.linkbubble.util.CrashTracking;
+
+public class NotificationOpenTabActivity extends Activity {
+
+    public static final String EXTRA_DISMISS_NOTIFICATION = BuildConfig.PACKAGE_NAME + ".notification";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        CrashTracking.init(this);
+
+        MainController mainController = MainController.get();
+
+        Intent intent = getIntent();
+
+        if (mainController != null) {
+            Log.d("blerg", "*** handle openTabFromNotification:" + intent.getIntExtra(EXTRA_DISMISS_NOTIFICATION, -1));
+            mainController.openTabFromNotification(intent.getIntExtra(EXTRA_DISMISS_NOTIFICATION, -1));
+        }
+
+        finish();
+    }
+
+}
+
