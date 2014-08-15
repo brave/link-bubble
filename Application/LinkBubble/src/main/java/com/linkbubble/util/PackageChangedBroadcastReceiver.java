a50bd2c10b6fa09b85ee902b35d1c6ac54dea5fe
Chris Lacy <gdaychris@gmail.com>
2014-08-15 14:42:18 +1000

    Fix possible NPE

--- Application/LinkBubble/src/main/java/com/linkbubble/util/PackageChangedBroadcastReceiver.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/util/PackageChangedBroadcastReceiver.java
@@ -13,13 +13,13 @@ public class PackageChangedBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
-        Log.d(DRM.TAG, "action:" + action + ", data:" + intent.getData().toString());
+        String data = intent.getData() != null ? intent.getData().toString() : null;
+        Log.d(DRM.TAG, "action:" + action + ", data:" + data);
         if (Intent.ACTION_PACKAGE_REMOVED.equals(action) || Intent.ACTION_PACKAGE_ADDED.equals(action)) {
             if (Settings.get() != null) {
                 Settings.get().updateBrowsers();
             }
 
-            String data = intent.getData() != null ? intent.getData().toString() : null;
             if (data != null && data.contains(BuildConfig.PRO_PACKAGE_NAME) && MainApplication.sDrm != null) {
                 MainApplication.checkForProVersion(context.getApplicationContext());
                 Log.d(DRM.TAG, "checkForProVersion() finished");
