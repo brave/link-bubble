04eefbfd2f99baeace8469c70fab518173702900
Chris Lacy <gdaychris@gmail.com>
2014-08-15 15:55:40 +1000

    clean up color names for material design changes

--- Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
@@ -116,9 +116,6 @@ public class ContentView extends FrameLayout {
     private boolean mHandledAppPickerForCurrentUrl = false;
     private boolean mUsingLinkBubbleAsDefaultForCurrentUrl = false;
 
-    private static Paint sIndicatorPaint;
-    private static Paint sBorderPaint;
-
     public ContentView(Context context) {
         this(context, null);
     }
@@ -129,16 +126,6 @@ public class ContentView extends FrameLayout {
 
     public ContentView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
-        if (sIndicatorPaint == null) {
-            sIndicatorPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            sIndicatorPaint.setColor(getResources().getColor(R.color.content_toolbar_background));
-        }
-
-        if (sBorderPaint == null) {
-            sBorderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            sBorderPaint.setColor(getResources().getColor(R.color.bubble_border));
-        }
     }
 
     public long getTotalTrackedLoadTime() {
