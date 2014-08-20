13212b1bac6067d15d47a282e72071b5dc97f0f2
Chris Lacy <gdaychris@gmail.com>
2014-08-20 15:08:42 +1000

    Initial Pause/Resume functionality for ArticleRenderer

--- Application/LinkBubble/src/main/java/com/linkbubble/articlerender/ArticleRenderer.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/articlerender/ArticleRenderer.java
@@ -2,15 +2,21 @@ package com.linkbubble.articlerender;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.linkbubble.Constant;
+import com.linkbubble.MainApplication;
+import com.linkbubble.MainController;
+import com.linkbubble.Settings;
 import com.linkbubble.util.Util;
+import com.squareup.otto.Subscribe;
 
 
 public class ArticleRenderer {
@@ -22,14 +28,17 @@ public class ArticleRenderer {
         public void onShowBrowserPrompt();
         public void onFirstPageLoadStarted();
     }
-    
+
+    private Context mContext;
     private WebView mWebView;
     private boolean mIsDestroyed = false;
     private boolean mFirstPageLoadTriggered = false;
     private Controller mController;
 
     public ArticleRenderer(Context context, Controller controller, ArticleContent articleContent, View articleRendererPlaceholder) {
+        mContext = context;
         mController = controller;
+        Log.e(BATTERY_SAVE_TAG, "create: " + this.getClass().getSimpleName());
 
         mWebView = new WebView(context);
         mWebView.setLayoutParams(articleRendererPlaceholder.getLayoutParams());
@@ -44,6 +53,7 @@ public class ArticleRenderer {
         display(articleContent, false);
 
         Log.d("Article", "ArticleRenderer()");
+        MainApplication.registerForBus(context, this);
     }
 
     public void display(ArticleContent articleContent) {
@@ -58,6 +68,7 @@ public class ArticleRenderer {
     }
 
     public void destroy() {
+        MainApplication.unregisterForBus(mContext, this);
         mIsDestroyed = true;
         if (mWebView != null) {
             mWebView.destroy();
@@ -135,4 +146,62 @@ public class ArticleRenderer {
         }
     };
 
+    private static final String BATTERY_SAVE_TAG = "BatterySaveArticleRenderer";
+
+    private void webviewPause(String via) {
+        String msg = "PAUSE (" + via + ") ";
+        if (mWebView != null && mIsDestroyed == false) {
+            mWebView.onPause();
+        }
+        Log.d(BATTERY_SAVE_TAG, msg);
+    }
+
+    private void webviewResume(String via) {
+        String msg = "RESUME (" + via + ") ";
+        if (mWebView != null && mIsDestroyed == false) {
+            mWebView.onResume();
+        }
+        Log.d(BATTERY_SAVE_TAG, msg);
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onUserPresentEvent(MainController.UserPresentEvent event) {
+        switch (Settings.get().getWebViewBatterySaveMode()) {
+            case Default:
+                webviewResume("userPresent");
+                break;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onScreenOffEvent(MainController.ScreenOffEvent event) {
+        switch (Settings.get().getWebViewBatterySaveMode()) {
+            case Aggressive:
+            case Default:
+                webviewPause("screenOff");
+                break;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onBeginCollapseTransitionEvent(MainController.BeginCollapseTransitionEvent event) {
+        switch (Settings.get().getWebViewBatterySaveMode()) {
+            case Aggressive:
+                webviewPause("beginCollapse");
+                break;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onBeginExpandTransitionEvent(MainController.BeginExpandTransitionEvent event) {
+        switch (Settings.get().getWebViewBatterySaveMode()) {
+            case Aggressive:
+                webviewResume("beginExpand");
+                break;
+        }
+    }
 }
