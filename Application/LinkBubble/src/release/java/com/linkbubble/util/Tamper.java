eb3e026b0010fa198763a87900329935f13354e3
Chris Lacy <gdaychris@gmail.com>
2014-10-02 10:52:09 +1000

    Upgrade tamper checks

--- Application/LinkBubble/src/release/java/com/linkbubble/util/Tamper.java
+++ Application/LinkBubble/src/release/java/com/linkbubble/util/Tamper.java
@@ -2,11 +2,36 @@ package com.linkbubble.util;
 
 import android.content.Context;
 
+import dexguard.util.CertificateChecker;
+import dexguard.util.TamperDetector;
+
 public class Tamper {
 
     public static boolean isTweaked(Context context) {
-        int check = dexguard.util.TamperDetection.checkApk(context);
-        return check == 0 ? false : true;
+        // You can pick your own value or values for OK,
+        // to make the code less predictable.
+        final int OK = 545635645;
+
+        // Let the DexGuard utility library detect whether the apk has
+        // been modified or repackaged in any way (with jar, zip,
+        // jarsigner, zipalign, or any other tool), after DexGuard has
+        // packaged it. The return value is the value of the optional
+        // integer argument OK (default=0) if the apk is unchanged.
+        int apkChanged =
+                TamperDetector.checkApk(context, OK);
+
+        // Let the DexGuard utility library detect whether the apk has
+        // been re-signed with a different certificate, after DexGuard has
+        // packaged it.  The return value is the value of the optional
+        // integer argument OK (default=0) if the certificate is still
+        // the same.
+        int certificateChanged =
+                CertificateChecker.checkCertificate(context, OK);
+
+        if (apkChanged != OK || certificateChanged != OK) {
+            return true;
+        }
+        return false;
     }
 
 }
