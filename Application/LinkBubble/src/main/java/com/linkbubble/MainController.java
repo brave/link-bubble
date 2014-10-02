brave/link-bubble

commit 750c534cbd25855cb2e0835c3a50b0c9fde1c838

Logging to track lifecycle/user flow for Crashlytics

Application/LinkBubble/src/main/java/com/linkbubble/MainController.java
@@ -904,16 +904,16 @@ public class MainController implements Choreographer.FrameCallback {
     }
 
     public boolean closeTab(TabView tabView, Constant.BubbleAction action, boolean animateOff, boolean canShowUndoPrompt) {
-        CrashTracking.setBool("contentViewShowing", contentViewShowing());
-        CrashTracking.setInt("getActiveTabCount", getActiveTabCount());
-        CrashTracking.setInt("getVisibleTabCount", getVisibleTabCount());
-        CrashTracking.setString("action", action.toString());
-        CrashTracking.setBool("animateOff", animateOff);
-        CrashTracking.setBool("canShowUndoPrompt", canShowUndoPrompt);
+        int activeTabCount = getActiveTabCount();
+        int visibleTabCount = getVisibleTabCount();
+        boolean contentViewShowing = contentViewShowing();
+        CrashTracking.log("MainController.closeTab(): action:" + action.toString() + ", contentViewShowing:" + contentViewShowing
+                + ", visibleTabCount:" + visibleTabCount + ", activeTabCount:" + activeTabCount + ", canShowUndoPrompt:" + canShowUndoPrompt
+                + ", animateOff:" + animateOff + ", canShowUndoPrompt:" + canShowUndoPrompt);
         if (mBubbleFlowDraggable != null) {
             mBubbleFlowDraggable.closeTab(tabView, animateOff, action, tabView != null ? tabView.getTotalTrackedLoadTime() : -1);
         }
-        int activeTabCount = getActiveTabCount();
+
         showBadge(activeTabCount > 1 ? true : false);
         if (activeTabCount == 0) {
             hideBubbleDraggable();
@@ -1088,6 +1088,7 @@ public class MainController implements Choreographer.FrameCallback {
     }
 
     public boolean reloadAllTabs(Context context) {
+        CrashTracking.log("MainController.reloadAllTabs()");
         boolean reloaded = false;
         closeAllBubbles(false);
         final Vector<String> urls = Settings.get().loadCurrentTabs();

Application/LinkBubble/src/main/java/com/linkbubble/MainApplication.java
@@ -24,6 +24,7 @@ import com.linkbubble.db.HistoryRecord;
 import com.linkbubble.ui.Prompt;
 import com.linkbubble.util.ActionItem;
 import com.linkbubble.util.Analytics;
+import com.linkbubble.util.CrashTracking;
 import com.linkbubble.util.IconCache;
 import com.linkbubble.util.Tamper;
 import com.linkbubble.util.Util;
@@ -76,6 +77,7 @@ public class MainApplication extends Application {
         sDrm = new DRM(this);
 
         initTrialStartTime();
+        CrashTracking.log("MainApplication.onCreate()");
     }
 
     static String TRIAL_START_TIME = "lb_trialStartTime";
@@ -271,6 +273,7 @@ public class MainApplication extends Application {
         if (urls == null || urls.length == 0 || Tamper.isTweaked(context)) {
             return;
         }
+        CrashTracking.log("MainApplication.restoreLinks(), urls.length:" + urls.length);
         Intent serviceIntent = new Intent(context, MainService.class);
         serviceIntent.putExtra("cmd", "restore");
         serviceIntent.putExtra("urls", urls);
@@ -285,6 +288,7 @@ public class MainApplication extends Application {
             intent.setComponent(defaultBrowserComponentName);
             context.startActivity(intent);
             activityStarted = true;
+            CrashTracking.log("MainApplication.openInBrowser()");
         }
 
         if (activityStarted == false && showToastIfNoBrowser) {
@@ -318,6 +322,7 @@ public class MainApplication extends Application {
             if (urlLoadStartTime > -1) {
                 Settings.get().trackLinkLoadTime(System.currentTimeMillis() - urlLoadStartTime, Settings.LinkLoadType.AppRedirectBrowser, urlAsString);
             }
+            CrashTracking.log("MainApplication.loadIntent()");
             return true;
         } catch (SecurityException ex) {
             openIntent = new Intent(Intent.ACTION_VIEW);
@@ -329,6 +334,7 @@ public class MainApplication extends Application {
                 if (urlLoadStartTime > -1) {
                     Settings.get().trackLinkLoadTime(System.currentTimeMillis() - urlLoadStartTime, Settings.LinkLoadType.AppRedirectBrowser, urlAsString);
                 }
+                CrashTracking.log("MainApplication.loadIntent() [2]");
                 return true;
             } catch (SecurityException ex2) {
                 if (toastOnError) {
@@ -359,6 +365,7 @@ public class MainApplication extends Application {
         boolean result = false;
         if (actionType == Constant.ActionType.Share) {
             String consumePackageName = Settings.get().getConsumeBubblePackageName(action);
+            CrashTracking.log("MainApplication.handleBubbleAction() action:" + action.toString() + ", consumePackageName:" + consumePackageName);
             String consumeName = Settings.get().getConsumeBubbleActivityClassName(action);
 
             if (consumePackageName.equals(BuildConfig.PACKAGE_NAME) && consumeName.equals(Constant.SHARE_PICKER_NAME)) {
@@ -393,9 +400,12 @@ public class MainApplication extends Application {
                 Toast.makeText(context, R.string.consume_activity_not_found, Toast.LENGTH_LONG).show();
             }
         } else if (actionType == Constant.ActionType.View) {
-            result = MainApplication.loadIntent(context, Settings.get().getConsumeBubblePackageName(action),
+            String consumePackageName = Settings.get().getConsumeBubblePackageName(action);
+            CrashTracking.log("MainApplication.handleBubbleAction() action:" + action.toString() + ", consumePackageName:" + consumePackageName);
+            result = MainApplication.loadIntent(context, consumePackageName,
                     Settings.get().getConsumeBubbleActivityClassName(action), urlAsString, -1, true);
         } else if (action == Constant.BubbleAction.Close) {
+            CrashTracking.log("MainApplication.handleBubbleAction() action:" + action.toString());
             result = true;
         }
 
@@ -436,8 +446,16 @@ public class MainApplication extends Application {
         app.getBus().post(new HistoryRecord.ChangedEvent(historyRecord));
     }
 
+    private static final String sIgnoreClassName = MainController.DraggableBubbleMovedEvent.class.getSimpleName();
+    private static String sLastPostClassName = "";
     public static void postEvent(Context context, Object event) {
         MainApplication app = (MainApplication) context.getApplicationContext();
+        String simpleName = event.getClass().getSimpleName();
+        if (sLastPostClassName.equals(sIgnoreClassName) == false
+                || sLastPostClassName.equals(sIgnoreClassName) == false) {
+            CrashTracking.log("post(" + simpleName + ")");
+            sLastPostClassName = simpleName;
+        }
         app.getBus().post(event);
     }
 

Application/LinkBubble/src/main/java/com/linkbubble/MainService.java
@@ -53,13 +53,15 @@ public class MainService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        String cmd = intent != null ? intent.getStringExtra("cmd") : null;
+        CrashTracking.log("MainService.onStartCommand(), cmd:" + cmd);
+
         MainController mainController = MainController.get();
-        if (mainController == null || intent == null || intent.getStringExtra("cmd") == null) {
+        if (mainController == null || intent == null || cmd == null) {
             stopSelf();
             return START_NOT_STICKY;
         }
 
-        String cmd = intent.getStringExtra("cmd");
         long urlLoadStartTime = intent.getLongExtra("start_time", System.currentTimeMillis());
         if (cmd.compareTo("open") == 0) {
             String url = intent.getStringExtra("url");
@@ -107,6 +109,7 @@ public class MainService extends Service {
 
         super.onCreate();
         CrashTracking.init(this);
+        CrashTracking.log("MainService.onCreate()");
 
         showDefaultNotification();
 
@@ -121,6 +124,7 @@ public class MainService extends Service {
                     Settings.get().saveBubbleRestingPoint();
                     FlushCacheService.doCheck(MainService.this);
                     stopSelf();
+                    CrashTracking.log("MainService.onCreate(): onDestroy()");
                 }
             });
 
@@ -159,6 +163,7 @@ public class MainService extends Service {
         unregisterReceiver(mDialogReceiver);
         unregisterReceiver(mBroadcastReceiver);
         MainController.destroy();
+        CrashTracking.log("MainService.onDestroy()");
         super.onDestroy();
     }
 

Application/LinkBubble/src/main/java/com/linkbubble/db/DatabaseHelper.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 import com.linkbubble.Settings;
+import com.linkbubble.util.CrashTracking;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -103,7 +104,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         try {
             db.insert(TABLE_LINK_HISTORY, null, getContentValues(historyRecord));
+            CrashTracking.log("DatabaseHelper.addHistoryRecord() success");
         } catch (IllegalStateException ex) {
+            CrashTracking.log("DatabaseHelper.addHistoryRecord() IllegalStateException");
         }
         db.close();
     }
@@ -112,9 +115,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         try {
             ContentValues values = getContentValues(historyRecord);
+            String id = String.valueOf(historyRecord.getId());
             db.update(TABLE_LINK_HISTORY, values,
-                                KEY_ID + " = ?", new String[] { String.valueOf(historyRecord.getId()) });
+                                KEY_ID + " = ?", new String[] { id });
+            CrashTracking.log("DatabaseHelper.updateHistoryRecord() success, id:" + id);
         } catch (IllegalStateException ex) {
+            CrashTracking.log("DatabaseHelper.addHistoryRecord() IllegalStateException");
         }
         db.close();
     }
@@ -125,10 +131,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         SQLiteDatabase db = getWritableDatabase();
         try {
-            db.delete(TABLE_LINK_HISTORY, KEY_ID + " = ?", new String[]{String.valueOf(historyRecord.getId())});
+            String id = String.valueOf(historyRecord.getId());
+            db.delete(TABLE_LINK_HISTORY, KEY_ID + " = ?", new String[]{id});
             result = true;
             Log.d(TAG, "deleted historyRecord:" + historyRecord.toString());
+            CrashTracking.log("DatabaseHelper.deleteHistoryRecord() success, id:" + id);
         } catch (IllegalStateException ex) {
+            CrashTracking.log("DatabaseHelper.deleteHistoryRecord() IllegalStateException");
         }
         db.close();
 
@@ -266,11 +275,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                         byte[] byteArray = cursor.getBlob(2);
                         result = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
                         Log.d(TAG, "getFavicon() - fetched favicon for " + faviconUrl);
+                        CrashTracking.log("DatabaseHelper.getFavicon() success, id:" + id);
                     } else {
                         idToDelete = id;
                     }
                 }
             } catch (IllegalStateException ex) {    // #302
+                CrashTracking.log("DatabaseHelper.getFavicon() IllegalStateException");
             }
             cursor.close();
 
@@ -332,7 +343,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         try {
             String idAsString = String.valueOf(id);
             db.delete(TABLE_FAVICON_CACHE, KEY_ID + " = ?", new String[]{idAsString});
+            CrashTracking.log("DatabaseHelper.deleteFavicon() success, id:" + idAsString);
         } catch (IllegalStateException ex) {
+            CrashTracking.log("DatabaseHelper.deleteFavicon(): IllegalStateException");
         }
 
         db.close();
@@ -364,7 +377,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         try {
             db.insert(TABLE_FAVICON_CACHE, null, values);
+            CrashTracking.log("DatabaseHelper.addFaviconForUrl() success");
         } catch (IllegalStateException ex) {
+            CrashTracking.log("DatabaseHelper.addFaviconForUrl(): IllegalStateException");
         }
         db.close();
 

Application/LinkBubble/src/main/java/com/linkbubble/ui/BubbleDraggable.java
@@ -16,6 +16,7 @@ import com.linkbubble.Settings;
 import com.linkbubble.physics.Circle;
 import com.linkbubble.physics.Draggable;
 import com.linkbubble.physics.DraggableHelper;
+import com.linkbubble.util.CrashTracking;
 import com.linkbubble.util.Util;
 
 import java.net.MalformedURLException;
@@ -325,10 +326,13 @@ public class BubbleDraggable extends BubbleView implements Draggable {
     }
 
     private void doAnimateToContentView(boolean saveBubbleRestingPoint) {
+        CrashTracking.log("doAnimateToContentView()");
         if (mAnimActive) {
             if (mMode == Mode.ContentView) {
+                CrashTracking.log("doAnimateToContentView() mMode == Mode.ContentView, early exit");
                 return;
             } else {
+                CrashTracking.log("doAnimateToContentView() cancelAnimation()");
                 mDraggableHelper.cancelAnimation();
             }
         }
@@ -356,12 +360,15 @@ public class BubbleDraggable extends BubbleView implements Draggable {
             @Override
             public void onAnimationComplete() {
                 onAnimComplete();
-                if (mainController.getActiveTabCount() == 0) {
+                int activeCount = mainController.getActiveTabCount();
+                if (activeCount == 0) {
                     // Ensure we don't enter state where there are no tabs to display. Fix #448
                     MainApplication.postEvent(getContext(), new MainController.EndCollapseTransitionEvent());
                     MainApplication.postEvent(getContext(), new ExpandedActivity.MinimizeExpandedActivityEvent());
+                    CrashTracking.log("doAnimateToContentView(): onAnimationComplete(): getActiveTabCount()==0");
                 } else {
                     MainApplication.postEvent(getContext(), mEndExpandTransitionEvent);
+                    CrashTracking.log("doAnimateToContentView(): onAnimationComplete(): getActiveTabCount():" + activeCount);
                 }
             }
             @Override
@@ -426,6 +433,7 @@ public class BubbleDraggable extends BubbleView implements Draggable {
                     }
 
                     MainApplication.postEvent(getContext(), mBeginBubbleDragEvent);
+                    CrashTracking.log("BubbleDraggable.configure(): onActionDown() - start drag");
                 }
             }
 
@@ -499,6 +507,7 @@ public class BubbleDraggable extends BubbleView implements Draggable {
             @Override
             public void onActionUp(DraggableHelper.ReleaseEvent e) {
                 if (mTouchDown) {
+                    CrashTracking.log("BubbleDraggable.configure(): onActionUp() - end drag");
                     mDraggableHelper.cancelAnimation();
 
                     if (mHasMoved) {
@@ -507,6 +516,7 @@ public class BubbleDraggable extends BubbleView implements Draggable {
                             float threshold = Config.dpToPx(900.0f);
                             if (v > threshold) {
                                 doFlick(e.vx, e.vy);
+                                CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doFlick()");
                             } else {
                                 MainApplication.postEvent(getContext(), mEndBubbleDragEvent);
 
@@ -517,24 +527,30 @@ public class BubbleDraggable extends BubbleView implements Draggable {
 
                                 if (doBubbleView) {
                                     mMode = Mode.BubbleView;
+                                    CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doSnap()");
                                     doSnap();
                                 } else {
+                                    CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doAnimateToContentView() [mHasMoved==true]");
                                     doAnimateToContentView();
                                 }
                             }
                         } else {
                             MainApplication.postEvent(getContext(), mEndBubbleDragEvent);
+                            CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doSnapAction()");
                             doSnapAction(mCurrentSnapTarget.getAction());
                         }
                     } else {
                         MainApplication.postEvent(getContext(), mEndBubbleDragEvent);
 
                         if (mMode == Mode.BubbleView) {
+                            CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doAnimateToContentView() [mMode == Mode.BubbleView]");
                             doAnimateToContentView();
                         } else {
                             if (mMode == Mode.ContentView && mBubbleFlowDraggable.isExpanded() == false) {
+                                CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doAnimateToContentView() [mMode == Mode.ContentView]");
                                 doAnimateToContentView();
                             } else {
+                                CrashTracking.log("BubbleDraggable.configure(): onActionUp() - doAnimateToBubbleView()");
                                 doAnimateToBubbleView(0);
                             }
                         }
@@ -557,6 +573,7 @@ public class BubbleDraggable extends BubbleView implements Draggable {
         if (targetX != x0 || targetY != y0) {
             setTargetPos(targetX, targetY, (float)targetTime / 1000.f, DraggableHelper.AnimationType.LargeOvershoot, null);
         }
+        CrashTracking.log("BubbleDraggable.slideOnScreen()");
     }
 
     public void setBubbleFlowDraggable(BubbleFlowDraggable bubbleFlowDraggable) {

Application/LinkBubble/src/main/java/com/linkbubble/ui/BubbleFlowDraggable.java
@@ -17,6 +17,7 @@ import com.linkbubble.R;
 import com.linkbubble.Settings;
 import com.linkbubble.physics.Draggable;
 import com.linkbubble.physics.DraggableHelper;
+import com.linkbubble.util.CrashTracking;
 import com.linkbubble.util.VerticalGestureListener;
 
 import java.net.MalformedURLException;
@@ -206,6 +207,8 @@ public class BubbleFlowDraggable extends BubbleFlowView implements Draggable {
 
     @Override
     public boolean expand(long time, final AnimationEventListener animationEventListener) {
+
+        CrashTracking.log("BubbleFlowDraggable.expand(): time:" + time);
 
         if (isExpanded() == false && mCurrentTab != null) {
             // Ensure the centerIndex matches the current bubble. This should only *NOT* be the case when
@@ -463,9 +466,13 @@ public class BubbleFlowDraggable extends BubbleFlowView implements Draggable {
     }
 
     public void closeAllBubbles(boolean removeFromCurrentTabs) {
+        int closeCount = 0;
         for (View view : mViews) {
             closeTab(((TabView) view), false, false);
+            closeCount++;
         }
+
+        CrashTracking.log("closeAllbubbles(): closeCount:" + closeCount);
 
         mViews.clear();
         postClosedTab(removeFromCurrentTabs);
@@ -479,5 +486,6 @@ public class BubbleFlowDraggable extends BubbleFlowView implements Draggable {
 
     public void saveCurrentTabs() {
         Settings.get().saveCurrentTabs(mViews);
+        CrashTracking.log("saveCurrentTabs()");
     }
 }

Application/LinkBubble/src/main/java/com/linkbubble/ui/BubbleFlowView.java
@@ -16,6 +16,7 @@ import android.widget.HorizontalScrollView;
 import com.linkbubble.Config;
 import com.linkbubble.Constant;
 import com.linkbubble.MainController;
+import com.linkbubble.util.CrashTracking;
 import com.linkbubble.util.TranslateAnimationEx;
 import com.linkbubble.util.Util;
 import com.linkbubble.util.VerticalGestureListener;
@@ -477,6 +478,7 @@ public class BubbleFlowView extends HorizontalScrollView {
     }
 
     public void collapse(long time, AnimationEventListener animationEventListener) {
+        CrashTracking.log("BubbleFlowView.collapse(): time:" + time);
         if (mIsExpanded == false) {
             return;
         }

Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
@@ -45,6 +45,7 @@ import com.linkbubble.articlerender.ArticleContent;
 import com.linkbubble.articlerender.ArticleRenderer;
 import com.linkbubble.util.ActionItem;
 import com.linkbubble.util.Analytics;
+import com.linkbubble.util.CrashTracking;
 import com.linkbubble.util.PageInspector;
 import com.linkbubble.util.Util;
 import com.linkbubble.webrender.WebRenderer;
@@ -404,6 +405,7 @@ public class ContentView extends FrameLayout {
         @Override
         public void onPageStarted(final String urlAsString, Bitmap favIcon) {
             Log.d(TAG, "onPageStarted() - " + urlAsString);
+            CrashTracking.log("onPageStarted(), index:" + MainController.get().getTabIndex(mOwnerTabView));
 
             // Ensure that items opened in new tabs are redirected to a browser when not licensed, re #371, re #360
             if (mInitialUrlAsString.equals(Constant.NEW_TAB_URL) && DRM.allowProFeatures() == false) {
@@ -477,6 +479,7 @@ public class ContentView extends FrameLayout {
                                 new ActionItem.OnActionItemDefaultSelectedListener() {
                                     @Override
                                     public void onSelected(ActionItem actionItem, boolean always) {
+                                        CrashTracking.log("onPageStarted(): OnActionItemDefaultSelectedListener.onSelected()");
                                         boolean loaded = false;
                                         String appPackageName = context.getPackageName();
                                         for (ResolveInfo resolveInfo : resolveInfos) {
@@ -543,7 +546,7 @@ public class ContentView extends FrameLayout {
 
         @Override
         public void onPageFinished(String urlAsString) {
-
+            CrashTracking.log("onPageFinished(), index:" + MainController.get().getTabIndex(mOwnerTabView));
             if (mLifeState != LifeState.Alive) {
                 return;
             }
@@ -622,6 +625,7 @@ public class ContentView extends FrameLayout {
 
         @Override
         public void onCloseWindow() {
+            CrashTracking.log("WebRenderer.Controller.onCloseWindow()");
             MainController.get().closeTab(mOwnerTabView, true, true);
         }
 
@@ -777,6 +781,7 @@ public class ContentView extends FrameLayout {
 
         @Override
         public void onAppOpened() {
+            CrashTracking.log("mOnOpenInAppButtonClickListener.onAppOpened()");
             MainController.get().closeTab(mOwnerTabView, true, false);
             // L_WATCH: L currently lacks getRecentTasks(), so minimize here
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
@@ -898,6 +903,7 @@ public class ContentView extends FrameLayout {
                         }
 
                         case R.id.item_reload_page: {
+                            CrashTracking.log("R.id.item_reload_page");
                             mWebRenderer.resetPageInspector();
                             URL currentUrl = mWebRenderer.getUrl();
                             mEventHandler.onPageLoading(currentUrl);
@@ -930,6 +936,7 @@ public class ContentView extends FrameLayout {
                         }
 
                         case R.id.item_close_tab: {
+                            CrashTracking.log("R.id.item_close_tab");
                             MainController.get().closeTab(mOwnerTabView, MainController.get().contentViewShowing(), true);
                             break;
                         }
@@ -969,6 +976,7 @@ public class ContentView extends FrameLayout {
 
     private void onDownloadStart(String urlAsString) {
         openInBrowser(urlAsString);
+        CrashTracking.log("onDownloadStart()");
         MainController.get().closeTab(mOwnerTabView, true, false);
         // L_WATCH: L currently lacks getRecentTasks(), so minimize here
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
@@ -978,9 +986,11 @@ public class ContentView extends FrameLayout {
 
     private boolean onBackPressed() {
         if (mUrlStack.size() == 0) {
+            CrashTracking.log("onBackPressed() - closeTab()");
             MainController.get().closeTab(mOwnerTabView, true, true);
             return true;
         } else {
+            CrashTracking.log("onBackPressed() - go back");
             mWebRenderer.stopLoading();
             String urlBefore = mWebRenderer.getUrl().toString();
 
@@ -1391,6 +1401,7 @@ public class ContentView extends FrameLayout {
 
     private boolean openInBrowser(String urlAsString, boolean canShowUndoPrompt) {
         Log.d(TAG, "openInBrowser() - url:" + urlAsString);
+        CrashTracking.log("openInBrowser()");
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(urlAsString));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -1409,6 +1420,7 @@ public class ContentView extends FrameLayout {
     private boolean openInApp(ResolveInfo resolveInfo, String urlAsString) {
         Context context = getContext();
         if (MainApplication.loadResolveInfoIntent(getContext(), resolveInfo, urlAsString, mInitialUrlLoadStartTime)) {
+            CrashTracking.log("openInApp(): resolveInfo:" + resolveInfo.activityInfo.packageName);
             String title = String.format(context.getString(R.string.link_loaded_with_app),
                     resolveInfo.loadLabel(context.getPackageManager()));
             MainApplication.saveUrlInHistory(context, resolveInfo, urlAsString, title);

commit 12d3409eea76340aceac9a8e4813b18063229fbb

Fix last bubble not going off screen as it should
Bug was introduced in 750c534

Application/LinkBubble/src/main/java/com/linkbubble/MainController.java
@@ -904,16 +904,17 @@ public class MainController implements Choreographer.FrameCallback {
     }
 
     public boolean closeTab(TabView tabView, Constant.BubbleAction action, boolean animateOff, boolean canShowUndoPrompt) {
-        int activeTabCount = getActiveTabCount();
-        int visibleTabCount = getVisibleTabCount();
+
+
         boolean contentViewShowing = contentViewShowing();
         CrashTracking.log("MainController.closeTab(): action:" + action.toString() + ", contentViewShowing:" + contentViewShowing
-                + ", visibleTabCount:" + visibleTabCount + ", activeTabCount:" + activeTabCount + ", canShowUndoPrompt:" + canShowUndoPrompt
+                + ", visibleTabCount:" + getVisibleTabCount() + ", activeTabCount:" + getActiveTabCount() + ", canShowUndoPrompt:" + canShowUndoPrompt
                 + ", animateOff:" + animateOff + ", canShowUndoPrompt:" + canShowUndoPrompt);
         if (mBubbleFlowDraggable != null) {
             mBubbleFlowDraggable.closeTab(tabView, animateOff, action, tabView != null ? tabView.getTotalTrackedLoadTime() : -1);
         }
 
+        int activeTabCount = getActiveTabCount();
         showBadge(activeTabCount > 1 ? true : false);
         if (activeTabCount == 0) {
             hideBubbleDraggable();