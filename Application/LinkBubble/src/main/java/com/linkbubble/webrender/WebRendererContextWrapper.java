4083b55b350881e01568e5c1e666221498e8580e
Chris Lacy <gdaychris@gmail.com>
2014-10-14 15:39:18 +1000

    Move WebRendererContextWrapper to own file

---
+++ Application/LinkBubble/src/main/java/com/linkbubble/webrender/WebRendererContextWrapper.java
@@ -0,0 +1,66 @@
+package com.linkbubble.webrender;
+
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Resources;
+
+import com.linkbubble.Constant;
+import com.linkbubble.ui.ExpandedActivity;
+
+
+class WebRendererContextWrapper extends ContextWrapper {
+
+    public WebRendererContextWrapper(Context base) {
+        super(base);
+    }
+
+    @Override
+    public Resources.Theme getTheme() {
+        if (Constant.ACTIVITY_WEBVIEW_RENDERING && ExpandedActivity.get() != null) {
+            return ExpandedActivity.get().getTheme();
+        }
+        return super.getTheme();
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+        if (Constant.ACTIVITY_WEBVIEW_RENDERING && ExpandedActivity.get() != null) {
+            return ExpandedActivity.get().getSystemService(name);
+        }
+        return super.getSystemService(name);
+    }
+
+        /*
+        @Override
+        public void startIntentSender(IntentSender intent,
+                                      Intent fillInIntent, int flagsMask, int flagsValues, int extraFlags)
+                throws IntentSender.SendIntentException {
+            super.startIntentSender(intent, fillInIntent, flagsMask,
+                    flagsValues, extraFlags);
+        }
+
+        @Override
+        public void startIntentSender(IntentSender intent,
+                                      Intent fillInIntent, int flagsMask, int flagsValues, int extraFlags,
+                                      Bundle options) throws IntentSender.SendIntentException {
+            super.startIntentSender(intent, fillInIntent, flagsMask,
+                    flagsValues, extraFlags, options);
+        }
+
+        @Override
+        public void startActivity(Intent intent) {
+            Log.e("blerg", "startActivity() " + intent.toString());
+            super.startActivity(intent);
+        }
+
+        @Override
+        public void startActivity(Intent intent, Bundle options) {
+            Log.e("blerg", "startActivity(i,o) " + intent.toString());
+            super.startActivity(intent, options);
+        }*/
+
+    public Resources getResources() {
+        return getBaseContext().getResources();
+    }
+}
