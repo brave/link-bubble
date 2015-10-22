2ae318480c172de0f7095ebf14848a5ba227d084
Brian R. Bondy <netzen@gmail.com>
2015-10-22 16:49:26 -0400

    Add an Ad Block preference and integrate to JNI

--- Application/LinkBubble/src/main/java/com/linkbubble/Settings.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/Settings.java
@@ -83,6 +83,7 @@ public class Settings {
     public static final String PREFERENCE_INCOGNITO_MODE = "preference_incognito";
     public static final String PREFERENCE_WEBVIEW_BATTERY_SAVING_MODE = "preference_webview_battery_save_v2";
     public static final String PREFERENCE_TRACKINGPROTECTION_MODE = "preference_trackingprotection";
+    public static final String PREFERENCE_ADBLOCK_MODE = "preference_adblock";
 
     public static final String PREFERENCE_WEBVIEW_TEXT_ZOOM = "preference_webview_text_zoom2";
     public static final int     PREFERENCE_WEBVIEW_TEXT_ZOOM_MIN = 50;
@@ -715,6 +716,10 @@ public class Settings {
 
     public boolean isTrackingProtectionEnabled() {
         return mSharedPreferences.getBoolean(PREFERENCE_TRACKINGPROTECTION_MODE, false);
+    }
+
+    public boolean isAdBlockEnabled() {
+        return mSharedPreferences.getBoolean(PREFERENCE_ADBLOCK_MODE, false);
     }
 
     public void setWebViewBatterySaveMode(String mode) {
--- Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
@@ -60,6 +60,8 @@ import com.linkbubble.MainApplication;
 import com.linkbubble.MainController;
 import com.linkbubble.R;
 import com.linkbubble.Settings;
+import com.linkbubble.adblock.ABPFilterParser;
+import com.linkbubble.adblock.TrackingProtectionList;
 import com.linkbubble.articlerender.ArticleContent;
 import com.linkbubble.articlerender.ArticleRenderer;
 import com.linkbubble.util.ActionItem;
@@ -155,6 +157,8 @@ public class ContentView extends FrameLayout {
     private boolean mApplyAutoSuggestionToUrlString = true;
     private boolean mSetTheRealUrlString = true;
     private boolean mFirstTimeUrlTyped = true;
+    private ABPFilterParser mABPParser = new ABPFilterParser();
+
 
     public ContentView(Context context) {
         this(context, null);
@@ -169,6 +173,7 @@ public class ContentView extends FrameLayout {
 
         mContext = context;
         mLoadingString = getResources().getString(R.string.loading);
+        mABPParser.init();
     }
 
     public long getTotalTrackedLoadTime() {
@@ -544,6 +549,34 @@ public class ContentView extends FrameLayout {
     }
 
     WebRenderer.Controller mWebRendererController = new WebRenderer.Controller() {
+
+        @Override
+        public boolean shouldAdBlockUrl(String urlStr) {
+            if (!Settings.get().isAdBlockEnabled()) {
+                return false;
+            }
+            return mABPParser.shouldBlock(urlStr);
+        }
+
+        @Override
+        public boolean shouldTrackingProtectionBlockUrl(String urlStr) {
+            if (!Settings.get().isTrackingProtectionEnabled()) {
+                return false;
+            }
+            String host;
+            try {
+                host = new URL(urlStr).getHost();
+            } catch (Exception e) {
+                return false;
+            }
+
+            if (TrackingProtectionList.shouldBlockHost(host)) {
+                // Just return a blank bad resource;
+                return true;
+            }
+
+            return false;
+        }
 
         @Override
         public boolean shouldOverrideUrlLoading(String urlAsString, boolean viaUserInput) {
