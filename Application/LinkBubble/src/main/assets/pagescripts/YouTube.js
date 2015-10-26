f66ec2dd2d4421b04d97358db851e9c962251fdb
Kevin Grandon <kevingrandon@yahoo.com>
2015-10-26 16:27:18 -0700

    Format javascript according to StandardJS spec.

--- Application/LinkBubble/src/main/assets/pagescripts/YouTube.js
+++ Application/LinkBubble/src/main/assets/pagescripts/YouTube.js
@@ -1,23 +1,24 @@
-(function() {
-    function detectYoutubeEmbeds() {
-        var elems = document.getElementsByTagName('*'), i;
-        var YOUTUBE_EMBED_PREFIX = "//www.youtube.com/embed/";
-        var resultArray = null;
-        var resultCount = 0;
-        for (i in elems) {
-           var elem = elems[i];
-           if (elem.src != null && elem.src.indexOf(YOUTUBE_EMBED_PREFIX) != -1) {
-               if (resultArray == null) {
-                   resultArray = new Array();
-               }
-               resultArray[resultCount] = elem.src;
-               resultCount++;
-           }
+;(function () {
+  function detectYoutubeEmbeds () {
+    var elems = document.getElementsByTagName('*')
+    var i
+    var YOUTUBE_EMBED_PREFIX = '//www.youtube.com/embed/'
+    var resultArray = null
+    var resultCount = 0
+    for (i in elems) {
+      var elem = elems[i]
+      if (elem.src != null && elem.src.indexOf(YOUTUBE_EMBED_PREFIX) !== -1) {
+        if (resultArray == null) {
+          resultArray = [ ]
         }
-        if (resultCount > 0) {
-            LinkBubble.onYouTubeEmbeds(resultArray.toString());
-        }
+        resultArray[resultCount] = elem.src
+        resultCount++
+      }
     }
+    if (resultCount > 0) {
+      window.LinkBubble.onYouTubeEmbeds(resultArray.toString())
+    }
+  }
 
-    detectYoutubeEmbeds();
-})();
+  detectYoutubeEmbeds()
+})()
