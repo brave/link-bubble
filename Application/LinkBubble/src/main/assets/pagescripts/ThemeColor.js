75b9867dd589370466b799104b5d906c25425e2a
Kevin Grandon <kevingrandon@yahoo.com>
2015-10-26 16:27:18 -0700

    Format javascript according to semi-standard spec.
    Using semi-standard for now due to the way that the JS is stringified and exectued within WebViews. In this case it breaks without the semi-colons. We'll fix this when time permits, but this works for now and is not worth spending more time on. At least the majority of the syntax matches other projects.

--- Application/LinkBubble/src/main/assets/pagescripts/ThemeColor.js
+++ Application/LinkBubble/src/main/assets/pagescripts/ThemeColor.js
@@ -1,6 +1,6 @@
-(function() {
-    var themeColorTag = document.getElementsByTagName('meta')['theme-color'];
-    if (themeColorTag) {
-        LinkBubble.onThemeColor(themeColorTag.getAttribute('content'));
-    }
+(function () {
+  var themeColorTag = document.getElementsByTagName('meta')['theme-color'];
+  if (themeColorTag) {
+    window.LinkBubble.onThemeColor(themeColorTag.getAttribute('content'));
+  }
 })();
--- Application/LinkBubble/src/main/assets/pagescripts/TouchIcon.js
+++ Application/LinkBubble/src/main/assets/pagescripts/TouchIcon.js
@@ -1,21 +1,21 @@
-(function() {
-	var links = document.head.getElementsByTagName('link');
-	var linksArray = null;
-	var linksCount = 0;
-	for(var link in links){
-        if(links.hasOwnProperty(link)){
-            var l = links[link];
-            if (l.rel != null && l.rel.indexOf('apple-touch-icon') != -1) {
-                if (linksArray == null) {
-                    linksArray = new Array();
-                }
-                var s = "@@@" + l.rel + "," + l.href + "," + l.sizes + "###";
-                linksArray[linksCount] = s;
-                linksCount++;
-            }
+(function () {
+  var links = document.head.getElementsByTagName('link');
+  var linksArray = null;
+  var linksCount = 0;
+  for (var link in links) {
+    if (links.hasOwnProperty(link)) {
+      var l = links[link];
+      if (l.rel != null && l.rel.indexOf('apple-touch-icon') !== -1) {
+        if (linksArray == null) {
+          linksArray = [];
         }
-	}
-	if (linksCount > 0) {
-		LinkBubble.onTouchIconLinks(linksArray.toString());
-	}
+        var s = '@@@' + l.rel + ',' + l.href + ',' + l.sizes + '###';
+        linksArray[linksCount] = s;
+        linksCount++;
+      }
+    }
+  }
+  if (linksCount > 0) {
+    window.LinkBubble.onTouchIconLinks(linksArray.toString());
+  }
 })();
