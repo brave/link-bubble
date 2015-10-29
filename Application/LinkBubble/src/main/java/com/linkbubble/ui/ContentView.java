1934841cc644705818138f7fc2c92a3ef873e337
Brian R. Bondy <netzen@gmail.com>
2015-10-28 22:45:40 -0400

    Move existing TP third party host checks to apply to both TP and adblock
    You'll also notice that I trim off www. in this pull request. This is
    because some sites like www.speedtest.net are blocked by us and serve
    images from c.speedtest.net which would have been considered a thrid
    party url if it was not stripped.

--- Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/ContentView.java
@@ -545,7 +545,6 @@ public class ContentView extends FrameLayout {
     void setFaviconColor(Integer color) {
         updateColors(color);
     }
-
     WebRenderer.Controller mWebRendererController = new WebRenderer.Controller() {
 
         @Override
@@ -555,19 +554,11 @@ public class ContentView extends FrameLayout {
         }
 
         @Override
-        public boolean shouldTrackingProtectionBlockUrl(String baseHost, String urlStr) {
-            String host;
-            try {
-                host = new URL(urlStr).getHost();
-            } catch (Exception e) {
-                return false;
-            }
-
+        public boolean shouldTrackingProtectionBlockUrl(String baseHost, String host) {
             if (TrackingProtectionList.shouldBlockHost(baseHost, host)) {
                 // Just return a blank bad resource;
                 return true;
-        }
-
+            }
             return false;
         }
 
