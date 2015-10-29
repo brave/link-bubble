2fcf7b288ea0591017212289aaa1dbd274f5963c
Kevin Grandon <kevingrandon@yahoo.com>
2015-10-29 09:06:53 -0700

    Revert "Format javascript according to StandardJS spec."
    This reverts commit f66ec2dd2d4421b04d97358db851e9c962251fdb.
    
    This broke some JS functionality, so reverting for now and will revisit down the road.

--- Application/LinkBubble/src/main/assets/pagescripts/YouTube.js
+++ Application/LinkBubble/src/main/assets/pagescripts/YouTube.js
@@ -1,24 +1,23 @@
-;(function () {
-  function detectYoutubeEmbeds () {
-    var elems = document.getElementsByTagName('*')
-    var i
-    var YOUTUBE_EMBED_PREFIX = '//www.youtube.com/embed/'
-    var resultArray = null
-    var resultCount = 0
-    for (i in elems) {
-      var elem = elems[i]
-      if (elem.src != null && elem.src.indexOf(YOUTUBE_EMBED_PREFIX) !== -1) {
-        if (resultArray == null) {
-          resultArray = [ ]
+(function() {
+    function detectYoutubeEmbeds() {
+        var elems = document.getElementsByTagName('*'), i;
+        var YOUTUBE_EMBED_PREFIX = "//www.youtube.com/embed/";
+        var resultArray = null;
+        var resultCount = 0;
+        for (i in elems) {
+           var elem = elems[i];
+           if (elem.src != null && elem.src.indexOf(YOUTUBE_EMBED_PREFIX) != -1) {
+               if (resultArray == null) {
+                   resultArray = new Array();
+               }
+               resultArray[resultCount] = elem.src;
+               resultCount++;
+           }
         }
-        resultArray[resultCount] = elem.src
-        resultCount++
-      }
+        if (resultCount > 0) {
+            LinkBubble.onYouTubeEmbeds(resultArray.toString());
+        }
     }
-    if (resultCount > 0) {
-      window.LinkBubble.onYouTubeEmbeds(resultArray.toString())
-    }
-  }
 
-  detectYoutubeEmbeds()
-})()
+    detectYoutubeEmbeds();
+})();
