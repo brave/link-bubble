8f35e50cc24e19e15a2a23ba2818a2cb1107ecd0
Serg <serg.zhukovsky@gmail.com>
2016-01-05 14:53:54 -0500

    Added jni files for tracking protection
    Auditors: @bbondy

--- scripts/copyJNIFiles.js
+++ scripts/copyJNIFiles.js
@@ -13,3 +13,7 @@ cp -f node_modules/bloom-filter-cpp/hashFn.h ./Application/LinkBubble/src/main/j
 cp -f node_modules/hashset-cpp/HashSet.cpp ./Application/LinkBubble/src/main/jni/HashSet.cpp
 cp -f node_modules/hashset-cpp/HashSet.h ./Application/LinkBubble/src/main/jni/HashSet.h
 cp -f node_modules/hashset-cpp/HashItem.h ./Application/LinkBubble/src/main/jni/HashItem.h
+cp -f node_modules/tracking-protection/FirstPartyHost.h ./Application/LinkBubble/src/main/jni/FirstPartyHost.h
+cp -f node_modules/tracking-protection/TrackerData.h ./Application/LinkBubble/src/main/jni/TrackerData.h
+cp -f node_modules/tracking-protection/TPParser.h ./Application/LinkBubble/src/main/jni/TPParser.h
+cp -f node_modules/tracking-protection/TPParser.cpp ./Application/LinkBubble/src/main/jni/TPParser.cpp
