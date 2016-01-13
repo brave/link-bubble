26a691a46b448c6d65074cde8b7e0df19493ce72
Serg <serg.zhukovsky@gmail.com>
2016-01-13 09:52:27 -0500

    Hide broken images
    Auditors: @bbondy

---
+++ Application/LinkBubble/src/main/assets/pagescripts/HideBrokenImages.js
@@ -0,0 +1,9 @@
+(function () {
+  var images = document.querySelectorAll('img');
+
+  Array.from(images).forEach(img => {
+    if (!img.complete || typeof img.naturalWidth == "undefined" || img.naturalWidth == 0) {
+      img.style.visibility = 'hidden';
+    }
+  });
+})();
