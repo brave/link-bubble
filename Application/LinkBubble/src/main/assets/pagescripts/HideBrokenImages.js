7cf128bc110f0f8b2cdee61638ce97340f1f3c8a
Serg <serg.zhukovsky@gmail.com>
2016-01-13 14:06:14 -0500

    Corrections after code review
    Auditors: @bbondy

--- Application/LinkBubble/src/main/assets/pagescripts/HideBrokenImages.js
+++ Application/LinkBubble/src/main/assets/pagescripts/HideBrokenImages.js
@@ -2,7 +2,7 @@
   var images = document.querySelectorAll('img');
 
   Array.from(images).forEach(img => {
-    if (!img.complete || typeof img.naturalWidth == "undefined" || img.naturalWidth == 0) {
+    if (!img.complete || typeof img.naturalWidth === "undefined" || img.naturalWidth === 0) {
       img.style.visibility = 'hidden';
     }
   });
