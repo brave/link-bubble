eaf07ae851ad305393fda674b99efd101e0478d8
Serg <serg.zhukovsky@gmail.com>
2016-01-18 09:17:59 -0500

    Added new files

---
+++ Application/LinkBubble/src/main/java/com/linkbubble/Constant.java
@@ -0,0 +1,132 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.linkbubble;
+
+import android.content.Context;
+
+public class Constant {
+
+    public enum BubbleAction {
+        None,
+        Close,
+        BackButton,
+        ConsumeRight,
+        ConsumeLeft,
+        //LinkDoubleTap,
+    }
+
+    public enum ActionType {
+        Unknown,
+        View,
+        Share,
+    }
+
+    // If true, transfer the WebView to an Activity. Enables text selection and drop down items to work
+    public static boolean ACTIVITY_WEBVIEW_RENDERING = false;
+
+    // Make GW's changes per d46678694ab79ed7a4aec5e293beff9ae9a62382 optional
+    public static final boolean DYNAMIC_ANIM_STEP = true;
+
+    public static final boolean PROFILE_FPS = false;
+
+    public static final String INTENT_EXTRA_IGNORE_LAUNCH_ANIMATION = "com.android.launcher.intent.extra.shortcut.INGORE_LAUNCH_ANIMATION";
+
+    public static final boolean EXPANDED_ACTIVITY_DEBUG = false;
+
+    public static boolean SAVE_CURRENT_TABS = true;
+
+    public static final int TRIAL_TIME = 1000 * 60 * 60 * 24;
+
+    public static boolean ENABLE_FLUSH_CACHE_SERVICE = false;
+
+    public static boolean COVER_STATUS_BAR = false;//(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) ? true : false;
+    public static boolean BOTTOM_CANVAS_MASK = false;
+    public static boolean TOP_CANVAS_MASK = true;
+
+    public static final int BUBBLE_ANIM_TIME = 550;
+    public static final int BUBBLE_FLOW_ANIM_TIME = 333;
+    public static final int BUBBLE_SLIDE_ON_SCREEN_TIME = BUBBLE_FLOW_ANIM_TIME;
+    public static final int CANVAS_FADE_ANIM_TIME  = BUBBLE_FLOW_ANIM_TIME;
+    public static final int TARGET_BUBBLE_APPEAR_TIME  = 150;
+
+    public static final float BUBBLE_MODE_ALPHA = 1.f;
+
+    public static final int DESIRED_FAVICON_SIZE = 96;
+
+    public static final String WELCOME_MESSAGE_URL = "http://linkbubble.com/device/welcome.html";
+    public static final String WELCOME_MESSAGE_DISPLAY_URL = "linkbubble.com/device/welcome";
+
+    // When opening a link in a new tab, there is no reliable way to get the link to be loaded. Use this guy
+    // so we can determine when this is occurring, and not pollute the history. #280
+    public static final String NEW_TAB_URL = "http://ishouldbeusedbutneverseen55675.com";
+
+    public static final int AUTO_CONTENT_DISPLAY_DELAY = 200;
+
+    public static final int TOUCH_ICON_MAX_SIZE = 256;
+
+    public static final int EMPTY_WEBVIEW_CACHE_INTERVAL = 7 * 24 * 60 * 60 * 1000;
+
+    public static final String PRIVACY_POLICY_URL = "http://www.linkbubble.com/privacy";
+    public static final String TERMS_OF_SERVICE_URL = "http://www.linkbubble.com/terms";
+
+    public static final boolean DEBUG_SHOW_TARGET_REGIONS = false;
+
+    public static final String SHARE_PICKER_NAME = "com.linkbubble.SharePicker";
+
+    public static String getOSFlavor() {
+        int apiVersion = android.os.Build.VERSION.SDK_INT;
+        String flavor = "";
+        switch (apiVersion) {
+            case 15: flavor = "4.0"; break;
+            case 16: flavor = "4.1"; break;
+            case 17: flavor = "4.2"; break;
+            case 18: flavor = "4.3"; break;
+            case 19: flavor = "4.4"; break;
+            case 20: flavor = "4.5"; break;
+        }
+        return flavor;
+    }
+
+    public static String DEVICE_ID = "<unset>";
+    public static String getValidDeviceId() {
+        if (DEVICE_ID.equals("<unset>") || DEVICE_ID == null || DEVICE_ID.length() < 4) {
+            return null;
+        }
+        return DEVICE_ID;
+    }
+
+    private static String sSecureAndroidId = null;
+    public static String getSecureAndroidId(Context context) {
+        if (sSecureAndroidId == null) {
+            sSecureAndroidId = android.provider.Settings.Secure.getString(context.getApplicationContext().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+        }
+        return sSecureAndroidId;
+    }
+
+    // String to represent the group all the notifications will be a part of
+    public static final String NOTIFICATION_GROUP_KEY_ARTICLES = "group_key_articles";
+
+    public static final String DATA_USER_ENTRY = "User";
+    public static final String DATA_USER_EMAIL_KEY_PREFIX = "email_";
+    public static final String DATA_USER_TWITTER_KEY_PREFIX = "twitter_";
+    public static final String DATA_USER_YAHOO_KEY_PREFIX = "yahoo_";
+    public static final int    DATA_USER_MAX_EMAILS = 9;
+
+    public static final String TWITTER_ACCOUNT_TYPE = "com.twitter.android.auth.login";
+    public static final String YAHOO_ACCOUNT_TYPE = "com.yahoo.mobile.client.share.account";
+
+    public static final String DATA_TRIAL_ENTRY = "Trial";
+    public static final String DATA_TRIAL_EMAIL = "email";
+
+    public static final String WEBVIEW_DATABASE_LOCATION = "/data/data/" + BuildConfig.APPLICATION_ID;
+
+    public static final String USER_AGENT_CHROME_PHONE = "Mozilla/5.0 (Linux; Android 4.4.2; GT-I9505 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36";
+    public static final String USER_AGENT_CHROME_TABLET = "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 7 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.48 Safari/537.36";
+    public static final String USER_AGENT_CHROME_DESKTOP = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36";
+
+    public static final String POCKET_PACKAGE_NAME = "com.ideashower.readitlater.pro";
+
+    public static final String ABOUT_BLANK_URI = "about:blank";
+}
---
+++ Application/LinkBubble/src/main/java/com/linkbubble/util/YouTubeEmbedHelper.java
@@ -0,0 +1,421 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package com.linkbubble.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ResolveInfo;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.linkbubble.Config;
+import com.linkbubble.MainApplication;
+import com.linkbubble.MainController;
+import com.linkbubble.R;
+import com.linkbubble.Settings;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YouTubeEmbedHelper {
+
+    private Context mContext;
+    private List<String> mEmbedIds = new ArrayList<String>();
+
+    private static class EmbedInfo {
+        String mTitle;
+        String mThumbnailUrl;
+        String mId;
+    }
+    private List<EmbedInfo> mEmbedInfo = new ArrayList<EmbedInfo>();
+    private DownloadYouTubeEmbedInfoTask mCurrentDownloadTask;
+
+    public ResolveInfo mYouTubeResolveInfo;
+
+    public YouTubeEmbedHelper(Context context) {
+        mContext = context;
+        mYouTubeResolveInfo = Settings.get().getYouTubeViewResolveInfo();
+    }
+
+    public void clear() {
+        mEmbedIds.clear();
+        if (mCurrentDownloadTask != null) {
+            synchronized (mCurrentDownloadTask) {
+                if (mCurrentDownloadTask != null) {
+                    mCurrentDownloadTask.cancel(true);
+                }
+                mCurrentDownloadTask = null;
+            }
+        }
+    }
+
+    public int size() {
+        return mEmbedIds.size();
+    }
+
+    /*
+     * Known YouTube embed URLs:
+        * http://www.youtube.com/embed/oSAW1tSNIa4?version=3&rel=1&fs=1&showsearch=0&showinfo=1&iv_load_policy=1&wmode=transparent
+        * https://www.youtube.com/embed/q1dpQKntj_w
+     */
+    public boolean onEmbeds(String[] strings) {
+        if (strings == null || strings.length == 0) {
+            return false;
+        }
+
+        boolean listChanged = false;
+
+        for (String string : strings) {
+            int prefixStartIndex = string.indexOf(Config.YOUTUBE_EMBED_PREFIX);
+            if (prefixStartIndex > -1) {
+                URL url;
+                try {
+                    url = new URL(string);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    break;
+                }
+
+                String path = url.getPath();
+                int pathStartIndex = path.indexOf(Config.YOUTUBE_EMBED_PATH_SUFFIX);
+                if (pathStartIndex > -1) {
+                    String videoId = path.substring(pathStartIndex + Config.YOUTUBE_EMBED_PATH_SUFFIX.length());
+                    if (videoId.length() > 0) {
+                        boolean onList = false;
+                        if (mEmbedIds.size() > 0) {
+                            for (String s : mEmbedIds) {
+                                if (s.equals(videoId)) {
+                                    onList = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (onList == false) {
+                            mEmbedIds.add(videoId);
+                            listChanged = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (listChanged) {
+            if (mCurrentDownloadTask != null) {
+                synchronized (mCurrentDownloadTask) {
+                    if (mCurrentDownloadTask != null) {
+                        mCurrentDownloadTask.cancel(true);
+                    }
+                    mCurrentDownloadTask = new DownloadYouTubeEmbedInfoTask(false, null);
+                    mCurrentDownloadTask.execute(null, null, null);
+                }
+            } else {
+                mCurrentDownloadTask = new DownloadYouTubeEmbedInfoTask(false, null);
+                mCurrentDownloadTask.execute(null, null, null);
+            }
+        }
+
+        return mEmbedIds.size() > 0;
+    }
+
+    private boolean loadYouTubeVideo(String id) {
+        if (mYouTubeResolveInfo != null) {
+            if (MainApplication.loadIntent(mContext, mYouTubeResolveInfo.activityInfo.packageName,
+                    mYouTubeResolveInfo.activityInfo.name, Config.YOUTUBE_WATCH_PREFIX + id, -1, true)) {
+                // L_WATCH: L currently lacks getRecentTasks(), so minimize here
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+                    MainController.get().switchToBubbleView();
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean onOpenInAppButtonClick() {
+        int size = mEmbedIds.size();
+        if (size == 1) {
+            return loadYouTubeVideo(mEmbedIds.get(0));
+        } else if (size > 1) {
+            Util.showThemedDialog(getMultipleEmbedsDialog());
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean embedInfoMatchesIds() {
+        if (mEmbedIds.size() == mEmbedInfo.size()) {
+            for (String id : mEmbedIds) {
+                boolean found = false;
+                for (EmbedInfo info : mEmbedInfo) {
+                    if (info.mId.equals(id)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == false) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private AlertDialog getMultipleEmbedsDialog() {
+        if (embedInfoMatchesIds()) {
+            return getEmbedResultsDialog();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+
+            LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.view_loading, null);
+
+            TextView textView = (TextView) view.findViewById(R.id.loading_text);
+            textView.setText(R.string.loading_youtube_embed_info);
+
+            builder.setView(view);
+            builder.setIcon(0);
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+
+            if (mCurrentDownloadTask != null) {
+                synchronized (mCurrentDownloadTask) {
+                    if (mCurrentDownloadTask != null) {
+                        mCurrentDownloadTask.cancel(true);
+                    }
+                    mCurrentDownloadTask = new DownloadYouTubeEmbedInfoTask(true, alertDialog);
+                    mCurrentDownloadTask.execute(null, null, null);
+                }
+            } else {
+                mCurrentDownloadTask = new DownloadYouTubeEmbedInfoTask(true, alertDialog);
+                mCurrentDownloadTask.execute(null, null, null);
+            }
+
+            return alertDialog;
+        }
+    }
+
+    AlertDialog getEmbedResultsDialog() {
+        if (mEmbedInfo.size() > 0) {
+            ListView listView = new ListView(mContext);
+            listView.setAdapter(new EmbedItemAdapter());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setView(listView);
+            builder.setIcon(mYouTubeResolveInfo.loadIcon(mContext.getPackageManager()));
+            builder.setTitle(R.string.title_youtube_embed_to_load);
+
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    EmbedInfo embedInfo = (EmbedInfo) view.getTag();
+                    if (embedInfo != null) {
+                        loadYouTubeVideo(embedInfo.mId);
+                    }
+                    alertDialog.dismiss();
+                }
+            });
+
+            return alertDialog;
+        } else {
+            final AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+            alertDialog.setTitle(R.string.youtube_embed_error_title);
+            alertDialog.setMessage(mContext.getString(R.string.youtube_embed_error_summary));
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, mContext.getResources().getString(R.string.action_ok), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    alertDialog.dismiss();
+                }
+
+            });
+            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            return alertDialog;
+        }
+    }
+
+    private class DownloadYouTubeEmbedInfoTask extends AsyncTask<Void, Void, Boolean> {
+        private AlertDialog mLoadingAlertDialog;
+        private boolean mShowResultsOnCompletion;
+
+        DownloadYouTubeEmbedInfoTask(boolean showResultsOnCompletion, AlertDialog loadingAlertDialog) {
+            super();
+            mShowResultsOnCompletion = showResultsOnCompletion;
+            mLoadingAlertDialog = loadingAlertDialog;
+        }
+
+        protected Boolean doInBackground(Void... arg) {
+
+            // This only should happen on a page change, in which case, abort
+            if (mEmbedIds.size() == 0 || isCancelled()) {
+                return false;
+            }
+
+            if (mCurrentDownloadTask != null) {
+                synchronized (mCurrentDownloadTask) {
+                    if (mCurrentDownloadTask != this) {
+                        return false;
+                    }
+                }
+            }
+
+            String idsAsString = "";
+            for (String id : mEmbedIds) {
+                if (idsAsString.length() > 0) {
+                    idsAsString += ",";
+                }
+                idsAsString += id;
+            }
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            String thumbnailsArg;
+            if (Config.isLowMemoryDevice()
+                || connectivityManager.isActiveNetworkMetered()
+                || NetworkConnectivity.isConnectedFast(mContext) == false) {
+                thumbnailsArg = Config.YOUTUBE_API_THUMBNAILS_LOW_QUALITY;
+            } else {
+                thumbnailsArg = Config.YOUTUBE_API_THUMBNAILS_HIGH_QUALITY;
+            }
+
+            String url = "https://www.googleapis.com/youtube/v3/videos?id=" + idsAsString + "&key=" + Config.YOUTUBE_API_KEY +
+                    "&part=snippet&fields=items(id,snippet(title," + thumbnailsArg + "))";
+
+            mEmbedInfo.clear();
+            String jsonAsString = Util.downloadJSONAsString(url, 5000);
+            if (jsonAsString != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonAsString);
+                    Object itemsObject = jsonObject.get("items");
+                    if (itemsObject instanceof JSONArray) {
+                        JSONArray jsonArray = (JSONArray)itemsObject;
+                        for (int i = 0; i < jsonArray.length(); ++i) {
+                            JSONObject item = jsonArray.getJSONObject(i);
+                            EmbedInfo embedInfo = new EmbedInfo();
+                            embedInfo.mId = item.getString("id");
+                            JSONObject snippet = item.getJSONObject("snippet");
+                            if (snippet != null) {
+                                embedInfo.mTitle = snippet.getString("title");
+                                JSONObject thumbnails = snippet.getJSONObject("thumbnails");
+                                if (thumbnails != null) {
+                                    try {
+                                        JSONObject mediumEntry = thumbnails.getJSONObject("medium");
+                                        if (mediumEntry != null) {
+                                            embedInfo.mThumbnailUrl = mediumEntry.getString("url");
+                                            mEmbedInfo.add(embedInfo);
+                                        }
+                                    } catch (JSONException e) {
+                                      // ignore "org.json.JSONException: No value for medium"
+                                    }
+
+                                    // Fallback to checking for default...
+                                    if (embedInfo.mThumbnailUrl == null) {
+                                        JSONObject defaultEntry = thumbnails.getJSONObject("default");
+                                        if (defaultEntry != null) {
+                                            embedInfo.mThumbnailUrl = defaultEntry.getString("url");
+                                            mEmbedInfo.add(embedInfo);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (mCurrentDownloadTask != null) {
+                synchronized (mCurrentDownloadTask) {
+                    if (mCurrentDownloadTask != this) {
+                        return false;
+                    }
+                }
+            }
+
+            if (isCancelled()) {
+                return false;
+            }
+
+            return mEmbedInfo.size() > 0 ? true : false;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            if (result.booleanValue() == true && mShowResultsOnCompletion && isCancelled() == false) {
+                mLoadingAlertDialog.dismiss();
+                mLoadingAlertDialog = null;
+
+                Util.showThemedDialog(getEmbedResultsDialog());
+            }
+        }
+    }
+
+    private class EmbedItemAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mEmbedInfo.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mEmbedInfo.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.view_youtube_embed_item, null);
+            }
+
+            EmbedInfo embedInfo = mEmbedInfo.get(position);
+
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.image);
+            if (embedInfo.mThumbnailUrl != null) {
+                Picasso.with(mContext).load(embedInfo.mThumbnailUrl).into(imageView);
+            }
+
+            TextView textView = (TextView)convertView.findViewById(R.id.text);
+            textView.setText(embedInfo.mTitle);
+
+            convertView.setTag(embedInfo);
+
+            return convertView;
+        }
+    }
+}
