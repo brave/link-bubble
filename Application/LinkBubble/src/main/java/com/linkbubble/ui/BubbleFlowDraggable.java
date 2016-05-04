7b2d4394f264d6bcc8cb877d9bf2434a6900d6aa
Serg <serg.zhukovsky@gmail.com>
2016-05-04 14:15:34 -0400

    Made Brave always be in recent app

--- Application/LinkBubble/src/main/java/com/linkbubble/ui/BubbleFlowDraggable.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/ui/BubbleFlowDraggable.java
@@ -365,10 +365,6 @@ public class BubbleFlowDraggable extends BubbleFlowView implements Draggable {
         }
 
         if (mViews.size() > 0) {
-            /*Intent intentActivity = new Intent(getContext(), BubbleFlowActivity.class);
-            intentActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            getContext().startActivity(intentActivity);*/
-
             Intent intent = new Intent(BubbleFlowActivity.ACTIVITY_INTENT_NAME);
             intent.putExtra("command", BubbleFlowActivity.EXPAND);
             LocalBroadcastManager bm = LocalBroadcastManager.getInstance(getContext());
