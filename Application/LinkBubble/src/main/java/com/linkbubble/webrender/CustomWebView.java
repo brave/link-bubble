6076aeb86a759ee648fa1070fce23b1b5efa21d4
Serg <serg.zhukovsky@gmail.com>
2016-05-12 13:53:12 -0400

    Fixed copy/paste from WebView on Lollipop

--- Application/LinkBubble/src/main/java/com/linkbubble/webrender/CustomWebView.java
+++ Application/LinkBubble/src/main/java/com/linkbubble/webrender/CustomWebView.java
@@ -7,10 +7,8 @@ package com.linkbubble.webrender;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.webkit.WebView;
 
-import com.linkbubble.MainController;
 
 public class CustomWebView extends WebView {
     private OnScrollChangedCallback mOnScrollChangedCallback;
